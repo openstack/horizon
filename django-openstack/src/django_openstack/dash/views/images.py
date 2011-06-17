bf3642f7297916b8e600718f1a615ea122f0835d
termie <github@anarkystic.com>
2011-06-17 16:46:40 -0700

    remove a bunch of unnecessary files

--- django-openstack/src/django_openstack/dash/views/images.py
+++ django-openstack/src/django_openstack/dash/views/images.py
@@ -30,9 +30,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render_to_response
 from django.utils.translation import ugettext as _
-from django_openstack.nova import exceptions
-from django_openstack.nova import forms as nova_forms
-#from django_openstack.nova import shortcuts
 from django import shortcuts
 
 from django_openstack import api
@@ -41,7 +38,7 @@ from openstackx.api import exceptions as api_exceptions
 from glance.common import exception as glance_exception
 
 
-LOG = logging.getLogger('django_openstack.nova')
+LOG = logging.getLogger('django_openstack.dash')
 
 
 class LaunchForm(forms.SelfHandlingForm):
@@ -114,132 +111,3 @@ def launch(request, tenant_id, image_id):
         'image': image,
         'form': form,
     }, context_instance=template.RequestContext(request))
-
-
-
-
-
-
-
-@login_required
-def detail(request, tenant_id, image_id):
-    image = api.compute_api(request).images.get(image_id)
-    tenant = api.get_tenant(request, request.user.tenant)
-    images = api.glance_api(request).get_images_detailed()
-
-    if not image:
-        raise http.Http404()
-    return render_to_response('django_openstack/nova/images/index.html', {
-        'form': nova_forms.LaunchForm(),
-        #'region': project.region,
-        'tenant': tenant,
-        'image_lists': _image_lists(images, tenant_id),
-        'image': image,
-    }, context_instance=template.RequestContext(request))
-
-
-# TODO(termie): below = NotImplemented
-
-@login_required
-def remove(request, project_id, image_id):
-    project = shortcuts.get_project_or_404(request, project_id)
-
-    if request.method == 'POST':
-        try:
-            project.deregister_image(image_id)
-        except exceptions.NovaApiError, e:
-            messages.error(request,
-                           _('Unable to deregister image: %s') % e.message)
-            LOG.error('Unable to deregister image "%s" from project "%s".'
-                      ' Exception message: "%s"' %
-                      (image_id, project_id, e.message))
-        else:
-            messages.success(request,
-                            _('Image %s has been successfully deregistered.') %
-                             image_id)
-            LOG.info('Image "%s" deregistered from project "%s"' %
-                     (image_id, project_id))
-
-    return shortcuts.redirect('nova_images', project_id)
-
-
-@login_required
-def privacy(request, project_id, image_id):
-    project = shortcuts.get_project_or_404(request, project_id)
-
-    if request.method == 'POST':
-        ami = project.get_image(image_id)
-
-        if ami.is_public:
-            try:
-                project.modify_image_attribute(image_id,
-                                               attribute='launchPermission',
-                                               operation='remove')
-                LOG.info('Image "%s" on project "%s" set to private' %
-                         (image_id, project_id))
-            except exceptions.NovaApiError, e:
-                messages.error(request,
-                               _('Unable to make image private: %s') %
-                                e.message)
-                LOG.error('Unable to make image "%s" private on project "%s".'
-                          ' Exception text: "%s"' %
-                          (image_id, project_id, e.message))
-        else:
-            try:
-                project.modify_image_attribute(image_id,
-                                               attribute='launchPermission',
-                                               operation='add')
-                LOG.info('Image "%s" on project "%s" set to public' %
-                         (image_id, project_id))
-            except exceptions.NovaApiError, e:
-                messages.error(request,
-                               _('Unable to make image public: %s') %
-                               e.message)
-                LOG.error('Unable to make image "%s" public on project "%s".'
-                          ' Exception text: "%s"' %
-                          (image_id, project_id, e.message))
-
-    return shortcuts.redirect('nova_images_detail', project_id, image_id)
-
-
-@login_required
-def update(request, project_id, image_id):
-    project = shortcuts.get_project_or_404(request, project_id)
-    ami = project.get_image(image_id)
-
-    if request.method == 'POST':
-        form = nova_forms.UpdateImageForm(ami, request.POST)
-        if form.is_valid():
-            try:
-                project.update_image(image_id,
-                                     form.cleaned_data['nickname'],
-                                     form.cleaned_data['description'])
-                LOG.info('Image "%s" on project "%s" updated' %
-                         (image_id, project_id))
-            except exceptions.NovaApiError, e:
-                messages.error(request,
-                               _('Unable to update image: %s') % e.message)
-            else:
-                messages.success(request,
-                                 _('Image %s has been updated.') % image_id)
-
-            return shortcuts.redirect('nova_images_detail', project_id, image_id)
-
-        # TODO(devcamcar): This needs to be cleaned up. Can make
-        # one of the render_to_response blocks go away.
-        else:
-            form = nova_forms.UpdateImageForm(ami)
-            return render_to_response('django_openstack/images/edit.html', {
-                'form': form,
-                'region': project.region,
-                'project': project,
-                'ami': ami,
-            }, context_instance=template.RequestContext(request))
-    else:
-        form = nova_forms.UpdateImageForm(ami)
-        return render_to_response('django_openstack/nova/images/edit.html', {
-            'form': form,
-            'region': project.region,
-            'project': project,
-            'ami': ami,
-        }, context_instance=template.RequestContext(request))
