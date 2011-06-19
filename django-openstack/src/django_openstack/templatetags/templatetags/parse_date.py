1831aff0eff563b44f0a4c8de073b156c33fecd0
Anthony Young <sleepsonthefloor@gmail.com>
2011-06-18 23:45:54 -0700

    need to handle more time formats apparently

--- django-openstack/src/django_openstack/templatetags/templatetags/parse_date.py
+++ django-openstack/src/django_openstack/templatetags/templatetags/parse_date.py
@@ -8,13 +8,16 @@ from django import template
 register = template.Library()
 
 class ParseDateNode(template.Node):
-   def render(self, context):
-       """Turn an iso formatted time back into a datetime."""
-       if context == None:
-           return "None"
-       else:
-           date_obj = datetime.datetime.strptime(context, "%Y-%m-%dT%H:%M:%S.%f")
-           return date_obj.strftime("%d/%m/%y at %H:%M:%S")
+    def render(self, context):
+        """Turn an iso formatted time back into a datetime."""
+        if context == None:
+            return "None"
+        else:
+            try:
+                date_obj = datetime.datetime.strptime(context, "%Y-%m-%dT%H:%M:%S.%f")
+            except:
+                date_obj = datetime.datetime.strptime(context, "%Y-%m-%d %H:%M:%S.%f")
+        return date_obj.strftime("%d/%m/%y at %H:%M:%S")
 
 
 @register.filter(name='parse_date')
