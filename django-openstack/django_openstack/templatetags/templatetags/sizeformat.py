fa992d3f6ae2365a17e26c223ffcfda5e5e84bff
Anthony Young <sleepsonthefloor@gmail.com>
2011-06-27 13:44:20 -0700

    diskgbformat

--- django-openstack/django_openstack/templatetags/templatetags/sizeformat.py
+++ django-openstack/django_openstack/templatetags/templatetags/sizeformat.py
@@ -7,10 +7,16 @@ from django import template
 from django.utils import translation
 from django.utils import formats
 
+
 register = template.Library()
 
-def int_format(number):
-    return int(number)
+def int_format(value):
+    return int(value)
+
+
+def float_format(value):
+    return formats.number_format(round(value, 1), 0)
+
 
 def filesizeformat(bytes, filesize_number_format):
     try:
@@ -18,10 +24,8 @@ def filesizeformat(bytes, filesize_number_format):
     except (TypeError,ValueError,UnicodeDecodeError):
         return translation.ungettext("%(size)d byte", "%(size)d bytes", 0) % {'size': 0}
 
-#    filesize_number_format = lambda value: formats.number_format(round(value, 1), 0)
-
     if bytes < 1024:
-        return translation.ungettext("%(size)d byte", "%(size)d bytes", bytes) % {'size': bytes}
+        return translation.ungettext("%(size)d", "%(size)d", bytes) % {'size': bytes}
     if bytes < 1024 * 1024:
         return translation.ugettext("%s KB") % filesize_number_format(bytes / 1024)
     if bytes < 1024 * 1024 * 1024:
@@ -32,6 +36,12 @@ def filesizeformat(bytes, filesize_number_format):
         return translation.ugettext("%s TB") % filesize_number_format(bytes / (1024 * 1024 * 1024 * 1024))
     return translation.ugettext("%s PB") % filesize_number_format(bytes / (1024 * 1024 * 1024 * 1024 * 1024))
 
+
 @register.filter(name='mbformat')
 def mbformat(mb):
     return filesizeformat(mb * 1024 * 1024, int_format).replace(' ', '')
+
+
+@register.filter(name='diskgbformat')
+def diskgbformat(gb):
+    return filesizeformat(gb * 1000 * 1000, float_format).replace(' ', '')
