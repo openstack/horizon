20e48bb3e7a34d7c30212b4ba83bed58492f12bd
Anthony Young <sleepsonthefloor@gmail.com>
2011-07-06 12:24:17 -0700

    improve error handling when keystone and other services are down

--- django-openstack/django_openstack/context_processors.py
+++ django-openstack/django_openstack/context_processors.py
@@ -1,8 +1,15 @@
-
 from django_openstack import api
+from django.contrib import messages
+from openstackx.api import exceptions as api_exceptions
 
 
 def tenants(request):
     if not request.user or not request.user.is_authenticated():
         return {}
-    return {'tenants': api.token_list_tenants(request, request.user.token)}
+
+    try:
+        return {'tenants': api.token_list_tenants(request, request.user.token)}
+    except api_exceptions.ApiException, e:
+        messages.error(request, "Unable to retrieve tenant list from\
+                                  keystone: %s" % e.message)
+        return {'tenants': []}
