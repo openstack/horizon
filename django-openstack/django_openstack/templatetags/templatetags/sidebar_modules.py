62bd21d114b0663ac31f9d68616d200bbcabc5e5
Jake Dahn <admin@jakedahn.com>
2011-08-01 17:59:06 -0700

    making active links work properly in sidebar for modules

--- django-openstack/django_openstack/templatetags/templatetags/sidebar_modules.py
+++ django-openstack/django_openstack/templatetags/templatetags/sidebar_modules.py
@@ -6,12 +6,12 @@ register = template.Library()
 
 
 @register.inclusion_tag('_sidebar_module.html')
-def dash_sidebar_modules():
+def dash_sidebar_modules(request):
     if signals.dash_apps_detection()[0][1]['type'] == "dash":
-        return {'modules': [module[1] for module in signals.dash_apps_detection()] }
+        return {'modules': [module[1] for module in signals.dash_apps_detection()], 'request': request }
 
 @register.inclusion_tag('_sidebar_module.html')
-def syspanel_sidebar_modules():
+def syspanel_sidebar_modules(request):
     if signals.dash_apps_detection()[0][1]['type'] == "syspanel":
-        return {'modules': [module[1] for module in signals.dash_apps_detection()] }
+        return {'modules': [module[1] for module in signals.dash_apps_detection()], 'request': request }
     
