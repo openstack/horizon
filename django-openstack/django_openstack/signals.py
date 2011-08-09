7dfa3191c3f5e82394df02657d499b42c4f5a276
Jake Dahn <admin@jakedahn.com>
2011-08-09 16:04:11 -0700

    adding a bit of documentation

--- django-openstack/django_openstack/signals.py
+++ django-openstack/django_openstack/signals.py
@@ -9,6 +9,16 @@ def dash_apps_detect():
     """
     Sends a pinging signal to the app, all listening modules will reply with
     items for the sidebar.
+    
+    The response is a tuple of the Signal object instance, and a dictionary.
+    The values within the dictionary containing links and a title which should
+    be added to the sidebar navigation.
+    
+    Example: (<dash_apps_ping>,
+              {'title': 'Nixon', 
+               'links': [{'url':'/syspanel/nixon/google', 
+                          'text':'Google', 'active_text': 'google'}],
+               'type': syspanel})
     """
     return dash_apps_ping.send(sender=dash_apps_ping)
 
