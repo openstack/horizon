44bdcff2ef66cc72cbc3373fda1e7b2c09c9ccdb
Arvind Somya <asomya@cisco.com>
2011-08-16 17:06:47 -0400

    Made quantum networking configurable.
    Added a context processor and check to make quantum networking configurable in the dashboard.

--- openstack-dashboard/dashboard/settings.py
+++ openstack-dashboard/dashboard/settings.py
@@ -64,6 +64,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.messages.context_processors.messages',
     'django_openstack.context_processors.swift',
     'django_openstack.context_processors.tenants',
+    'django_openstack.context_processors.quantum',
 )
 
 TEMPLATE_LOADERS = (
