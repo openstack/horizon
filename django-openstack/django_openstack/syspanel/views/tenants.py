8aab55de3b083dce6f9ca720b9235651c4436ee0
Jesse Andrews <anotherjesse@gmail.com>
2011-09-27 05:47:50 +0000

    add/remove user from tenant improvements

--- django-openstack/django_openstack/syspanel/views/tenants.py
+++ django-openstack/django_openstack/syspanel/views/tenants.py
@@ -22,6 +22,7 @@ from django import template
 from django import http
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.shortcuts import redirect
 from django.utils.translation import ugettext as _
@@ -59,7 +60,7 @@ class AddUser(forms.SelfHandlingForm):
         except api_exceptions.ApiException, e:
             messages.error(request, 'Unable to create user association: %s' %
                            (e.message))
-        return redirect('syspanel_tenants')
+        return redirect(reverse('syspanel_tenant_users', kwargs={'tenant_id': data['tenant']}))
 
 
 class RemoveUser(forms.SelfHandlingForm):
@@ -79,7 +80,7 @@ class RemoveUser(forms.SelfHandlingForm):
         except api_exceptions.ApiException, e:
             messages.error(request, 'Unable to create tenant: %s' %
                            (e.message))
-        return redirect('syspanel_tenants')
+        return redirect(reverse('syspanel_tenant_users', kwargs={'tenant_id': data['tenant']}))
 
 
 class CreateTenant(forms.SelfHandlingForm):
@@ -243,22 +244,15 @@ def users(request, tenant_id):
 
     users = api.account_api(request).users.get_for_tenant(tenant_id)
     all_users = api.account_api(request).users.list()
-    new_user_ids = []
     user_ids = [u.id for u in users]
-    all_user_ids = [u.id for u in all_users]
-    for uid in all_user_ids:
-        if not uid in user_ids:
-            new_user_ids.append(uid)
-    for i in user_ids:
-        if i in new_user_ids:
-            new_user_ids.remove(i)
+    new_users = [u for u in all_users if not u.id in user_ids]
     return render_to_response(
     'django_openstack/syspanel/tenants/users.html', {
         'add_user_form': add_user_form,
         'remove_user_form': remove_user_form,
         'tenant_id': tenant_id,
         'users': users,
-        'new_users': new_user_ids,
+        'new_users': new_users,
     }, context_instance=template.RequestContext(request))
 
 
