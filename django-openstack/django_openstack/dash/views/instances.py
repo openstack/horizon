2925fc306b7d1027d0893aa72feead4a574e3c82
Jake Dahn <admin@jakedahn.com>
2011-10-17 13:43:50 -0700

    don't need fancy tail method anymore

--- django-openstack/django_openstack/dash/views/instances.py
+++ django-openstack/django_openstack/dash/views/instances.py
@@ -110,29 +110,6 @@ class UpdateInstance(forms.SelfHandlingForm):
         return shortcuts.redirect('dash_instances', tenant_id)
 
 
-def tail(f, n, offset=None):
-    """Reads a n lines from f with an offset of offset lines.  The return
-    value is a tuple in the form ``(lines, has_more)`` where `has_more` is
-    an indicator that is `True` if there are more lines in the file.
-    """
-    avg_line_length = 74
-    to_read = n + (offset or 0)
-
-    while 1:
-        try:
-            f.seek(-(avg_line_length * to_read), 2)
-        except IOError:
-            # woops.  apparently file is smaller than what we want
-            # to step back, go to the beginning instead
-            f.seek(0)
-        pos = f.tell()
-        lines = f.read().splitlines()
-        if len(lines) >= to_read or pos == 0:
-            return lines[-to_read:offset and -offset or None], \
-                   len(lines) > to_read or pos > 0
-        avg_line_length *= 1.3
-
-
 @login_required
 def index(request, tenant_id):
     for f in (TerminateInstance, RebootInstance):
