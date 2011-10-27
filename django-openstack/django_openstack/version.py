11fa157ef39cf80b00de422e5d4fe7c50a2d5ac1
Joe Heck <heckj@mac.com>
2011-10-27 13:37:04 -0700

    fixing NOVA -> Horizon

--- django-openstack/django_openstack/version.py
+++ django-openstack/django_openstack/version.py
@@ -19,8 +19,8 @@ version_info = {'branch_nick': u'LOCALBRANCH',
                 'revno': 0}
 
 
-NOVA_VERSION = ['2012', '1']
-YEAR, COUNT = NOVA_VERSION
+HORIZON_VERSION = ['2012', '1']
+YEAR, COUNT = HORIZON_VERSION
 FINAL = False   # This becomes true at Release Candidate time
 
 
@@ -33,11 +33,3 @@ def version_string():
         return canonical_version_string()
     else:
         return '%s-dev' % (canonical_version_string(),)
-
-
-def vcs_version_string():
-    return '%s:%s' % (version_info['branch_nick'], version_info['revision_id'])
-
-
-def version_string_with_vcs():
-    return '%s-%s' % (canonical_version_string(), vcs_version_string())
