96f5225f6d7863e907f67d7ea0226065557d5e55
Neil Johnston <onewheeldrive.net@gmail.com>
2011-12-13 16:17:20 -0800

    blueprint Improve Dashboard UX
    bug 904030
    
    After the user submits a form by hitting a submit button (an application wide behaviour), the button now provides visual confrimation of it's submission by disabing though the removal of the primary class.
    
    Change-Id: If52fe30b55b78a46951568df7a78dc2b6efa6ad3

--- openstack-dashboard/dashboard/static/dashboard/js/application.js
+++ openstack-dashboard/dashboard/static/dashboard/js/application.js
@@ -95,6 +95,8 @@ $(function(){
       $(this).submit(function() {
           return false;
       });
+      $('input:submit').removeClass('primary').addClass('disabled');
+      $('input:submit').attr('disabled', 'disabled');
       return true;
   });
 
