fd9d583c47abeec7d6111a0e4ce407df0e251dd8
Paul McMillan <paul.mcmillan@nebula.com>
2012-01-23 19:13:52 -0800

    Converted Quantum to Datatables.
    This is known to be broken with current quantum APIs,
    but it is a proper refactor of the previous tables using
    the new DataTables, modals, and exception handling.
    
    It makes future updates for API compatibility much easier.
    
    Change-Id: I9690db8553773f40d8aad88ffde90b26815bfed2

--- horizon/horizon/dashboards/nova/networks/forms.py
+++ horizon/horizon/dashboards/nova/networks/forms.py
@@ -25,6 +25,7 @@ from django.contrib import messages
 from django.utils.translation import ugettext as _
 
 from horizon import api
+from horizon import exceptions
 from horizon import forms
 
 
@@ -36,44 +37,14 @@ class CreateNetwork(forms.SelfHandlingForm):
 
     def handle(self, request, data):
         network_name = data['name']
-
         try:
-            LOG.info('Creating network %s ' % network_name)
-            send_data = {'network': {'name': '%s' % network_name}}
-            api.quantum_create_network(request, send_data)
-        except Exception, e:
-            if not hasattr(e, 'message'):
-                e.message = str(e)
-            messages.error(request,
-                           _('Unable to create network %(network)s: %(msg)s') %
-                           {"network": network_name, "msg": e.message})
-            return shortcuts.redirect(request.build_absolute_uri())
-        else:
-            msg = _('Network %s has been created.') % network_name
-            LOG.info(msg)
-            messages.success(request, msg)
-            return shortcuts.redirect('horizon:nova:networks:index')
-
-
-class DeleteNetwork(forms.SelfHandlingForm):
-    network = forms.CharField(widget=forms.HiddenInput())
-
-    def handle(self, request, data):
-        try:
-            LOG.info('Deleting network %s ' % data['network'])
-            api.quantum_delete_network(request, data['network'])
-        except Exception, e:
-            if not hasattr(e, 'message'):
-                e.message = str(e)
-            messages.error(request,
-                    _('Unable to delete network %(network)s: %(msg)s') %
-                    {"network": data['network'], "msg": e.message})
-        else:
-            msg = _('Network %s has been deleted.') % data['network']
-            LOG.info(msg)
-            messages.success(request, msg)
-
-        return shortcuts.redirect(request.build_absolute_uri())
+            data = {'network': {'name': network_name}}
+            api.quantum.quantum_create_network(request, data)
+            messages.success(request,
+                             _('Network %s has been created.') % network_name)
+        except:
+            exceptions.handle(request, _("Unable to create network."))
+        return shortcuts.redirect('horizon:nova:networks:index')
 
 
 class RenameNetwork(forms.SelfHandlingForm):
@@ -98,7 +69,7 @@ class RenameNetwork(forms.SelfHandlingForm):
             LOG.info(msg)
             messages.success(request, msg)
 
-        return shortcuts.redirect(request.build_absolute_uri())
+        return shortcuts.redirect('horizon:nova:networks:index')
 
 
 class CreatePort(forms.SelfHandlingForm):
@@ -123,30 +94,8 @@ class CreatePort(forms.SelfHandlingForm):
             LOG.info(msg)
             messages.success(request, msg)
 
-        return shortcuts.redirect(request.build_absolute_uri())
-
-
-class DeletePort(forms.SelfHandlingForm):
-    network = forms.CharField(widget=forms.HiddenInput())
-    port = forms.CharField(widget=forms.HiddenInput())
-
-    def handle(self, request, data):
-        try:
-            LOG.info('Deleting %s ports on network %s' %
-                     (data['port'], data['network']))
-            api.quantum_delete_port(request, data['network'], data['port'])
-        except Exception, e:
-            if not hasattr(e, 'message'):
-                e.message = str(e)
-            messages.error(request,
-                           _('Unable to delete port %(port)s: %(msg)s') %
-                           {"port": data['port'], "msg": e.message})
-        else:
-            msg = _('Port %(port)s deleted from network %(network)s.') % {
-                  "port": data['port'], "network": data['network']}
-            LOG.info(msg)
-            messages.success(request, msg)
-        return shortcuts.redirect(request.build_absolute_uri())
+        return shortcuts.redirect('horizon:nova:networks:detail',
+                                  network_id=data['network'])
 
 
 class AttachPort(forms.SelfHandlingForm):
@@ -170,68 +119,13 @@ class AttachPort(forms.SelfHandlingForm):
 
     def handle(self, request, data):
         try:
-            LOG.info('Attaching %s port to VIF %s' %
-                     (data['port'], data['vif_id']))
             body = {'attachment': {'id': '%s' % data['vif_id']}}
             api.quantum_attach_port(request,
-                                        data['network'], data['port'], body)
-        except Exception, e:
-            if not hasattr(e, 'message'):
-                e.message = str(e)
-            messages.error(request,
-                _('Unable to attach port %(port)s to VIF %(vif)s: %(msg)s') %
-                {"port": data['port'],
-                 "vif": data['vif_id'],
-                 "msg": e.message})
-        else:
-            msg = _('Port %(port)s connected to VIF %(vif)s.') % \
-                  {"port": data['port'], "vif": data['vif_id']}
-            LOG.info(msg)
-            messages.success(request, msg)
-        return shortcuts.redirect(request.build_absolute_uri())
-
-
-class DetachPort(forms.SelfHandlingForm):
-    network = forms.CharField(widget=forms.HiddenInput())
-    port = forms.CharField(widget=forms.HiddenInput())
-
-    def handle(self, request, data):
-        try:
-            LOG.info('Detaching port %s' % data['port'])
-            api.quantum_detach_port(request, data['network'], data['port'])
-        except Exception, e:
-            if not hasattr(e, 'message'):
-                e.message = str(e)
-            messages.error(request,
-                _('Unable to detach port %(port)s: %(message)s') %
-                {"port": data['port'], "message": e.message})
-        else:
-            msg = _('Port %s detached.') % (data['port'])
-            LOG.info(msg)
-            messages.success(request, msg)
-        return shortcuts.redirect(request.build_absolute_uri())
-
-
-class TogglePort(forms.SelfHandlingForm):
-    network = forms.CharField(widget=forms.HiddenInput())
-    port = forms.CharField(widget=forms.HiddenInput())
-    state = forms.CharField(widget=forms.HiddenInput())
-
-    def handle(self, request, data):
-        try:
-            LOG.info('Toggling port state to %s' % data['state'])
-            body = {'port': {'state': '%s' % data['state']}}
-            api.quantum_set_port_state(request,
-                                       data['network'], data['port'], body)
-        except Exception, e:
-            if not hasattr(e, 'message'):
-                e.message = str(e)
-            messages.error(request,
-                _('Unable to set port state to %(state)s: %(message)s') %
-                {"state": data['state'], "message": e.message})
-        else:
-            msg = _('Port %(port)s state set to %(state)s.') % {
-                "port": data['port'], "state": data['state']}
-            LOG.info(msg)
-            messages.success(request, msg)
-        return shortcuts.redirect(request.build_absolute_uri())
+                                    data['network'],
+                                    data['port'],
+                                    body)
+            messages.success(request, _("Port attached."))
+        except:
+            exceptions.handle(request, _('Unable to attach port.'))
+        return shortcuts.redirect("horizon:nova:networks:detail",
+                                  data['network'])
---
+++ horizon/horizon/dashboards/nova/networks/tables.py
@@ -0,0 +1,118 @@
+import logging
+
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import api
+from horizon import tables
+
+
+LOG = logging.getLogger(__name__)
+
+
+class RenameNetworkLink(tables.LinkAction):
+    name = "rename_network"
+    verbose_name = _("Rename Network")
+    url = "horizon:nova:networks:rename"
+    attrs = {"class": "ajax-modal"}
+
+
+class CreateNetworkLink(tables.LinkAction):
+    name = "create_network"
+    verbose_name = _("Create New Network")
+    url = "horizon:nova:networks:create"
+    attrs = {"class": "ajax-modal btn small"}
+
+
+class DeleteNetworkAction(tables.DeleteAction):
+    data_type_singular = _("Network")
+    data_type_plural = _("Networks")
+
+    def delete(self, request, obj_id):
+        api.quantum_delete_network(request, obj_id)
+
+
+class NetworksTable(tables.DataTable):
+    id = tables.Column('id', verbose_name=_('Network Id'),
+                       link="horizon:nova:networks:detail")
+    name = tables.Column('name', verbose_name=_('Name'))
+    used = tables.Column('used', verbose_name=_('Used'))
+    available = tables.Column('available', verbose_name=_('Available'))
+    total = tables.Column('total', verbose_name=_('Total'))
+    #tenant = tables.Column('tenant', verbose_name=_('Tenant'))
+
+    def get_object_id(self, datum):
+        return datum['id']
+
+    def get_object_display(self, obj):
+        return obj['name']
+
+    class Meta:
+        name = "networks"
+        verbose_name = _("Networks")
+        row_actions = (DeleteNetworkAction, RenameNetworkLink,)
+        table_actions = (CreateNetworkLink, DeleteNetworkAction,)
+
+
+class CreatePortLink(tables.LinkAction):
+    name = "create_port"
+    verbose_name = _("Create Ports")
+    url = "horizon:nova:networks:port_create"
+    attrs = {"class": "ajax-modal btn small"}
+
+    def get_link_url(self, datum=None):
+        network_id = self.table.kwargs['network_id']
+        return reverse(self.url, args=(network_id,))
+
+
+class DeletePortAction(tables.DeleteAction):
+    data_type_singular = _("Port")
+    data_type_plural = _("Ports")
+
+    def delete(self, request, obj_id):
+        api.quantum_delete_port(request,
+                                self.table.kwargs['network_id'],
+                                obj_id)
+
+
+class DetachPortAction(tables.BatchAction):
+    name = "detach_port"
+    action_present = _("Detach")
+    action_past = _("Detached")
+    data_type_singular = _("Port")
+    data_type_plural = _("Ports")
+
+    def action(self, request, datum_id):
+        body = {'port': {'state': 'DOWN'}}
+        api.quantum_set_port_state(request,
+                                   self.table.kwargs['network_id'],
+                                   datum_id, body)
+
+
+class AttachPortAction(tables.LinkAction):
+    name = "attach_port"
+    verbose_name = _("Attach Port")
+    url = "horizon:nova:networks:port_attach"
+    attrs = {"class": "ajax-modal"}
+
+    def get_link_url(self, datum=None):
+        network_id = self.table.kwargs['network_id']
+        return reverse(self.url, args=(network_id, datum['id']))
+
+
+class NetworkDetailsTable(tables.DataTable):
+    id = tables.Column('id', verbose_name=_('Port Id'))
+    state = tables.Column('state', verbose_name=_('State'))
+    attachment = tables.Column('attachment', verbose_name=_('Attachment'))
+
+    def get_object_id(self, datum):
+        return datum['id']
+
+    def get_object_display(self, obj):
+        return obj['id']
+
+    class Meta:
+        name = "network_details"
+        verbose_name = _("Network Port Details")
+        row_actions = (DeletePortAction, AttachPortAction, DetachPortAction)
+        table_actions = (CreatePortLink, DeletePortAction,)
