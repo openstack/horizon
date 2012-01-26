61cdfb654dceaa7cfe617b571ed58be8dc9685a4
Paul McMillan <paul.mcmillan@nebula.com>
2012-01-25 23:44:15 -0800

    Separate time overrides from test machinery.
    This patch separates the ability to override time (for testing
    purposes) from the actual import of the test machinery. Horizon
    should never import test modules and libraries like mox or
    django.test.client unless tests are being run. Fixes 921943.
    
    Change-Id: I7638b0fdb51926acbd809f33a9c35443ce069713

--- horizon/horizon/test.py
+++ horizon/horizon/test.py
@@ -32,32 +32,9 @@ from horizon import context_processors
 from horizon import middleware
 from horizon import users
 
-
-def time():
-    '''Overrideable version of datetime.datetime.today'''
-    if time.override_time:
-        return time.override_time
-    return datetime.time()
-
-time.override_time = None
-
-
-def today():
-    '''Overridable version of datetime.datetime.today'''
-    if today.override_time:
-        return today.override_time
-    return datetime.datetime.today()
-
-today.override_time = None
-
-
-def utcnow():
-    '''Overridable version of datetime.datetime.utcnow'''
-    if utcnow.override_time:
-        return utcnow.override_time
-    return datetime.datetime.utcnow()
-
-utcnow.override_time = None
+from .time import time
+from .time import today
+from .time import utcnow
 
 
 class RequestFactoryWithMessages(RequestFactory):
