22acb8d142ef4c094c681f9e93598f1b51a33723
jakedahn <jake@ansolabs.com>
2012-03-05 21:47:22 -0800

    Making initial stride to improve openstack-esq design in sphinx.
    Change-Id: I4c015b66e818b876c7ce23cdd103b785680d5b58

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -216,7 +216,10 @@ html_theme = '_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "nosidebar": "false",
+    "sidebarwidth": "500px"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
