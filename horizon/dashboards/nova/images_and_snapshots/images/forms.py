63ca8238a59e9e22acac84334aa4594b1447976b
Andy Chong <andycjw@gmail.com>
2012-03-22 15:48:31 +0800

    Allow image name up to 255 characters
     * fix bug 960443
    
    Change-Id: I8e0e7f3e15180054888ea4d158f3ba5349a09c86

--- horizon/dashboards/nova/images_and_snapshots/images/forms.py
+++ horizon/dashboards/nova/images_and_snapshots/images/forms.py
@@ -39,7 +39,7 @@ LOG = logging.getLogger(__name__)
 
 class UpdateImageForm(forms.SelfHandlingForm):
     image_id = forms.CharField(widget=forms.HiddenInput())
-    name = forms.CharField(max_length="25", label=_("Name"))
+    name = forms.CharField(max_length="255", label=_("Name"))
     kernel = forms.CharField(max_length="36", label=_("Kernel ID"),
                              widget=forms.TextInput(
                                 attrs={'readonly': 'readonly'}
