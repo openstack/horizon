fb83d849d7a5ba471a60f0f3af43e8fb0e3f5ed2
Gabriel Hurley <gabriel@strikeawe.com>
2012-06-28 17:36:21 -0700

    Check for quota data before trying to use it in JS.
    Fixes bug 1019086.
    
    Change-Id: I1657db433b9ae2b9376f3d9e307580a6d32e1b7e

--- horizon/static/horizon/js/horizon.quota.js
+++ horizon/static/horizon/js/horizon.quota.js
@@ -108,20 +108,22 @@ horizon.Quota = {
   showFlavorDetails: function() {
     this.getSelectedFlavor();
 
-    var name = horizon.utils.truncate(this.selected_flavor.name, 14, true);
-    var vcpus = horizon.utils.humanizeNumbers(this.selected_flavor.vcpus);
-    var disk = horizon.utils.humanizeNumbers(this.selected_flavor.disk);
-    var ephemeral = horizon.utils.humanizeNumbers(this.selected_flavor["OS-FLV-EXT-DATA:ephemeral"]);
-    var disk_total = this.selected_flavor.disk + this.selected_flavor["OS-FLV-EXT-DATA:ephemeral"];
-    var disk_total_display = horizon.utils.humanizeNumbers(disk_total);
-    var ram = horizon.utils.humanizeNumbers(this.selected_flavor.ram);
+    if (this.selected_flavor) {
+      var name = horizon.utils.truncate(this.selected_flavor.name, 14, true);
+      var vcpus = horizon.utils.humanizeNumbers(this.selected_flavor.vcpus);
+      var disk = horizon.utils.humanizeNumbers(this.selected_flavor.disk);
+      var ephemeral = horizon.utils.humanizeNumbers(this.selected_flavor["OS-FLV-EXT-DATA:ephemeral"]);
+      var disk_total = this.selected_flavor.disk + this.selected_flavor["OS-FLV-EXT-DATA:ephemeral"];
+      var disk_total_display = horizon.utils.humanizeNumbers(disk_total);
+      var ram = horizon.utils.humanizeNumbers(this.selected_flavor.ram);
 
-    $("#flavor_name").html(name);
-    $("#flavor_vcpus").text(vcpus);
-    $("#flavor_disk").text(disk);
-    $("#flavor_ephemeral").text(ephemeral);
-    $("#flavor_disk_total").text(disk_total_display);
-    $("#flavor_ram").text(ram);
+      $("#flavor_name").html(name);
+      $("#flavor_vcpus").text(vcpus);
+      $("#flavor_disk").text(disk);
+      $("#flavor_ephemeral").text(ephemeral);
+      $("#flavor_disk_total").text(disk_total_display);
+      $("#flavor_ram").text(ram);
+    }
   },
 
   // Updates a progress bar, taking care of exceeding quota display as well.
@@ -162,7 +164,7 @@ horizon.Quota = {
         return;
       } else if(progress_stat === 'instances') {
         update_amount = instance_count;
-      } else {
+      } else if (scope.selected_flavor) {
         update_amount = (scope.selected_flavor[progress_stat] * instance_count);
       }
 
