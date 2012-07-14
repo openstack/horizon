b282751440955e5a912588ad97284b6336913f39
Tihomir Trifonov <t.trifonov@gmail.com>
2012-07-14 16:55:33 +0300

    Fixed logic for disabling form button
    This logic should be applied *only* for btn-primary
    submit buttons, and not for regular buttons, which are
    all of the action button in tables. At the moment this
    logic breaks the table row actions, as it sets
    button.disabled=true, and the 'action' value of the button
    is not submitted, hence no action is performed.
    
    Change-Id: I8e9827975656c53c7f8baff6554de1103bf4a358

--- horizon/static/horizon/js/horizon.forms.js
+++ horizon/static/horizon/js/horizon.forms.js
@@ -18,7 +18,7 @@ horizon.addInitFunction(function () {
   // Disable multiple submissions when launching a form.
   $("form").submit(function () {
     var button = $(this).find('[type="submit"]');
-    if (button.length && !button.hasClass('always-enabled')){
+    if (button.hasClass('btn-primary') && !button.hasClass('always-enabled')){
       $(this).submit(function () {
         return false;
       });
