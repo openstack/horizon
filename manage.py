9c77ce0fa95707aea2df2bd2e01cc0b3fa02f74c
lrqrun <lrqrun@gmail.com>
2012-08-29 15:34:24 +0800

    Fix PEP8 issues.
    Fix some pep8 issues in manage.py make the code looks pretty.
    
    Change-Id: I0e02ed538dbef9ab41a3c698e94ac56e4afc018e

--- manage.py
+++ manage.py
@@ -3,10 +3,9 @@
 import os
 import sys
 
+from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openstack_dashboard.settings")
-
-    from django.core.management import execute_from_command_line
-
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "openstack_dashboard.settings")
     execute_from_command_line(sys.argv)
