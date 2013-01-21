20fa0fcaa55074359f2db7e5eea64b9342a8fa75
Julie Pichon <jpichon@redhat.com>
2013-01-21 14:47:16 +0000

    Don't show the EC2 Credentials panel if there is no EC2 service
    Fixes bug #1102411
    
    Change-Id: I2454b415ac99f19349f4a19a1358fc75d8307fba

--- horizon/dashboards/settings/ec2/panel.py
+++ horizon/dashboards/settings/ec2/panel.py
@@ -23,6 +23,7 @@ from horizon.dashboards.settings import dashboard
 class EC2Panel(horizon.Panel):
     name = _("EC2 Credentials")
     slug = 'ec2'
+    permissions = ('openstack.services.ec2',)
 
 
 dashboard.Settings.register(EC2Panel)
