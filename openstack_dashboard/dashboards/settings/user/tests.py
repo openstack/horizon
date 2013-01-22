0d303c8e6d7b8ec831f3b3d304a3309d0e26a26a
Julie Pichon <jpichon@redhat.com>
2013-01-22 14:53:15 +0000

    Add UTC offset information to the timezone
    In the User Settings, indicate besides the timezone name what the
    actual timezone/UTC offset is.
    
    Fixes bug 1103003
    
    Change-Id: I8070df2bc0380da1555c40dc9394895fbfde6848

---
+++ openstack_dashboard/dashboards/settings/user/tests.py
@@ -0,0 +1,31 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.core.urlresolvers import reverse
+
+from openstack_dashboard.test import helpers as test
+
+
+INDEX_URL = reverse("horizon:settings:user:index")
+
+
+class UserSettingsTest(test.TestCase):
+
+    def test_timezone_offset_is_displayed(self):
+        res = self.client.get(INDEX_URL)
+
+        self.assertContains(res, "Australia/Melbourne (UTC +11:00)")
+        self.assertContains(res, "Canada/Newfoundland (UTC -03:30)")
