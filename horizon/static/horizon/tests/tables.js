openstack/horizon

commit 4632aa58ae9d1874f559031f592e2873c0fb4f64

Don't update count on tables without a summary row
The horizon.tabs javascript was updating the last row of any <table>
in the tab when the focus was changed to a different tab, stomping
over the last row of the table.

This commit limits updates to tables with the .datatable class,
and adds a class to the updated <span> in affected templates
so it can be selected a little more unambiguously.

Fixes bug #1080513.

Change-Id: I1b778bdc77887dcf540c96c2ab8be239bc13258c

horizon/static/horizon/js/horizon.tables.js
@@ -18,7 +18,7 @@ horizon.datatables = {
       // Trigger the update handlers.
       $rows_to_update.each(function(index, row) {
         var $row = $(this),
-            $table = $row.closest('table');
+            $table = $row.closest('table.datatable');
         horizon.ajax.queue({
           url: $row.attr('data-update-url'),
           error: function (jqXHR, textStatus, errorThrown) {
@@ -185,10 +185,10 @@ horizon.datatables.update_footer_count = function (el, modifier) {
   // code paths for table or browser footers...
   $browser = $el.closest("#browser_wrapper");
   if ($browser.length) {
-    $footer = $($browser.find('.tfoot span')[1]);
+    $footer = $browser.find('.tfoot span.content_table_count');
   }
   else {
-    $footer = $el.find('tr:last span:first');
+    $footer = $el.find('table.datatable tfoot span.table_count');
   }
   row_count = $el.find('tbody tr:visible').length + modifier - $el.find('.empty').length;
   footer_text_template = ngettext("Displaying %s item", "Displaying %s items", row_count);
@@ -198,7 +198,7 @@ horizon.datatables.update_footer_count = function (el, modifier) {
 
 horizon.datatables.set_table_sorting = function (parent) {
 // Function to initialize the tablesorter plugin strictly on sortable columns.
-$(parent).find("table.table").each(function () {
+$(parent).find("table.datatable").each(function () {
   var $table = $(this),
       header_options = {};
   // Disable if not sortable or has <= 1 item
@@ -289,7 +289,7 @@ horizon.datatables.set_table_filter = function (parent) {
 
 horizon.addInitFunction(function() {
   horizon.datatables.validate_button();
-  horizon.datatables.update_footer_count($.find('table'),0);
+  horizon.datatables.update_footer_count($.find('table.datatable'),0);
   // Bind the "select all" checkbox action.
   $('div.table_wrapper, #modal_wrapper').on('click', 'table thead .multi_select_column :checkbox', function(evt) {
     var $this = $(this),

horizon/static/horizon/js/horizon.tabs.js
@@ -34,7 +34,9 @@ horizon.addInitFunction(function () {
   $(document).on("shown", ".nav-tabs a[data-toggle='tab']", function (evt) {
     var $tab = $(evt.target),
         $content = $($(evt.target).attr('data-target'));
-    horizon.datatables.update_footer_count($content.find("table"));
+    $content.find("table.datatable").each(function () {
+        horizon.datatables.update_footer_count($(this));
+    });
     horizon.cookies.update("tabs", $tab.closest(".nav-tabs").attr("id"), $tab.attr('data-target'));
   });
 

horizon/templates/horizon/common/_data_table.html
@@ -3,7 +3,7 @@
 <div class="table_wrapper">
   {% if needs_form_wrapper %}<form action="{{ table.get_absolute_url }}" method="POST">{% csrf_token %}{% endif %}
   {% with columns=table.get_columns rows=table.get_rows %}
-  <table id="{{ table.name }}" class="table table-bordered table-striped">
+  <table id="{{ table.name }}" class="table table-bordered table-striped datatable">
     <thead>
       <tr class='table_caption'>
         <th class='table_header' colspan='{{ columns|length }}'>
@@ -50,7 +50,7 @@
       {% endif %}
       <tr>
         <td colspan="{{ table.get_columns|length }}">
-          <span>{% blocktrans count counter=rows|length %}Displaying {{ counter }} item{% plural %}Displaying {{ counter }} items{% endblocktrans %}</span>
+          <span class="table_count">{% blocktrans count counter=rows|length %}Displaying {{ counter }} item{% plural %}Displaying {{ counter }} items{% endblocktrans %}</span>
           {% if table.has_more_data %}
           <span class="spacer">|</span>
           <a href="?{{ table.get_pagination_string }}">More&nbsp;&raquo;</a>

commit 08071a20459d622f62c23808a421a31f2e1c1168

Fix footer count update
Commit 4632aa58ae9d1874f559031f592e2873c0fb4f64 broke the update
due to an incorrect selector.

Also adds a qunit regression test for footer count update.

Fixes bug #1114667.

Change-Id: Iee9a8743957e86985fcab70f7716e244e214b43e

horizon/static/horizon/js/horizon.tables.js
@@ -188,7 +188,7 @@ horizon.datatables.update_footer_count = function (el, modifier) {
     $footer = $browser.find('.tfoot span.content_table_count');
   }
   else {
-    $footer = $el.find('table.datatable tfoot span.table_count');
+    $footer = $el.find('tfoot span.table_count');
   }
   row_count = $el.find('tbody tr:visible').length + modifier - $el.find('.empty').length;
   footer_text_template = ngettext("Displaying %s item", "Displaying %s items", row_count);

horizon/templates/horizon/qunit.html
@@ -12,6 +12,7 @@
     <script type="text/javascript" src="{{ STATIC_URL }}horizon/tests/messages.js"></script>
     <script type="text/javascript" src="{{ STATIC_URL }}horizon/tests/modals.js"></script>
     <script type="text/javascript" src="{{ STATIC_URL }}horizon/tests/templates.js"></script>
+    <script type="text/javascript" src="{{ STATIC_URL }}horizon/tests/tables.js"></script>
     {% comment %}End test modules.{% endcomment %}
 
     {% include "horizon/_scripts.html" %}
@@ -24,10 +25,27 @@
     <ol id="qunit-tests"></ol>
     <div id="qunit-fixture">
       <!-- Test markup; will be hidden. -->
+
       <div id="main_content">
         <div class="messages"></div>
         <div id="modal_wrapper"></div>
       </div>
+
+      <table id="table1" class="datatable">
+        <tbody>
+          <tr id="cat1"><td>cat1</td></tr>
+          <tr id="dog1"><td>dog1</td></tr>
+          <tr id="cat2"><td>cat2</td></tr>
+          <tr id="dog2"><td>dog2</td></tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="1">
+              <span class="table_count">Displaying 4 items</span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
   </body>
 </html>

horizon/static/horizon/tests/tables.js
@@ -1,0 +1,29 @@
+module("Tables (horizon.tables.js)");
+
+test("Footer count update", function () {
+    var fixture = $("#qunit-fixture");
+    var table =  fixture.find("table.datatable");
+    var tbody = table.find('tbody');
+    var table_count = table.find("span.table_count");
+
+    horizon.datatables.update_footer_count(table);
+    notEqual(table_count.text().indexOf('4 items'), -1, "Initial count is correct");
+
+    // hide rows
+    $("table.datatable tbody tr#dog1").hide();
+    $("table.datatable tbody tr#cat2").hide();
+    horizon.datatables.update_footer_count(table);
+    notEqual(table_count.text().indexOf('2 items'), -1, "Count correct after hiding two rows");
+
+    // show a row
+    $("table.datatable tbody tr#cat2").show();
+    horizon.datatables.update_footer_count(table);
+    notEqual(table_count.text().indexOf('3 items'), -1, "Count correct after showing one row");
+
+    // add rows
+    $("table.datatable tbody tr#cat2").show();
+    $('<tr id="cat3"><td>cat3</td></tr>"').appendTo(tbody);
+    $('<tr id="cat4"><td>cat4</td></tr>"').appendTo(tbody);
+    horizon.datatables.update_footer_count(table);
+    notEqual(table_count.text().indexOf('5 items'), -1, "Count correct after adding two rows");
+});