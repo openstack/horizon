63203ab1069999f81b9518c11da29eb8ca537077
Julie Pichon <jpichon@redhat.com>
2013-02-18 12:33:54 +0000

    Resolve API import issues (quantum)
    Restore the imports in the api init file, without including the "from"
    to ensure api calls still must specify "api.nova", "api.quantum", etc.
    
    Fixes bug #1125632
    
    Change-Id: I981105ce0ed7f1352de42fe2c0620665ba378823

---
+++ openstack_dashboard/api/__init__.py
@@ -0,0 +1,42 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+#
+# Copyright 2012 Nebula, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Methods and interface objects used to interact with external APIs.
+
+API method calls return objects that are in many cases objects with
+attributes that are direct maps to the data returned from the API http call.
+Unfortunately, these objects are also often constructed dynamically, making
+it difficult to know what data is available from the API object.  Because of
+this, all API calls should wrap their returned object in one defined here,
+using only explicitly defined atributes and/or methods.
+
+In other words, Horizon developers not working on openstack_dashboard.api
+shouldn't need to understand the finer details of APIs for
+Keystone/Nova/Glance/Swift et. al.
+"""
+import base
+import cinder
+import glance
+import keystone
+import network
+import nova
+import quantum
+import swift
