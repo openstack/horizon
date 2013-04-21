91e4715c141874e6caeffb4056981efef9c781e1
Monty Taylor <mordred@inaugust.com>
2013-04-20 23:11:16 -0700

    Migrate to PBR for build tooling.
    pbr is the library-ified version of the code that was in
    openstack.common.setup and version. It builds on top of d2to1 to
    provide declarative build configuration.
    
    Change-Id: Ic264fa33e17fe5fdc71d8c53412495b069ab74a4

--- horizon/version.py
+++ horizon/version.py
@@ -14,6 +14,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from openstack_dashboard.openstack.common import version as common_version
+import pbr.version
 
-version_info = common_version.VersionInfo('horizon')
+version_info = pbr.version.VersionInfo('horizon')
