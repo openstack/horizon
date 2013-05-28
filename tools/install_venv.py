5b24518b10a842dcceb4236860ac3e80839f1789
Jiri Tomasek <jtomasek@redhat.com>
2013-05-28 10:06:10 +0200

    Renames tools/pip-requires to requirements.txt
    and tools/test-requires to test-requirements.txt
    
    These are standard files and tools in the
    general world are growing intelligence about them.
    
    Change-Id: Iac3c680dd19cabd8ea38598a6e2bb27238030a2d
    Fixes: bug #1179008

--- tools/install_venv.py
+++ tools/install_venv.py
@@ -32,8 +32,8 @@ import sys
 ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 VENV = os.path.join(ROOT, '.venv')
 WITH_VENV = os.path.join(ROOT, 'tools', 'with_venv.sh')
-PIP_REQUIRES = os.path.join(ROOT, 'tools', 'pip-requires')
-TEST_REQUIRES = os.path.join(ROOT, 'tools', 'test-requires')
+PIP_REQUIRES = os.path.join(ROOT, 'requirements.txt')
+TEST_REQUIRES = os.path.join(ROOT, 'test-requirements.txt')
 
 
 def die(message, *args):
