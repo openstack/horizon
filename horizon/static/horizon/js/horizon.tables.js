470743e89e4e106d593a67bced4f0aa8711e7264
Nicolas Simonds <nic@metacloud.com>
2013-06-04 15:25:46 -0700

    Make the multi-select button on the "Floating IPs" page work again
    This was a result of an assumption made by table-sorter.js, which
    was parroted by Horizon; that there will only be one row of table
    headers in a <thead> section.  Horizon uses two rows: one for the
    "table title" and one for the individual table headings.
    
    With the default settings to tablesorter(), this yielded an off-by-one
    error when applying custom sorters (or parsers, depending on which
    nomenclature you like, table-sorter.js uses two) to the table.
    
    it is only tickled when manually defining custom parsers for columns,
    and has been known about for a while, as there was code to compensate
    for the observed behaviour.
    
    Altering the xpath to omit the extra headers (and telling tablesorter
    to do the same) yields the expected results.  I would personally
    prefer to chain the results through .not(), but tablesorter only
    provides support for xpath
    
    Change-Id: I3338dd0cafd8271de6bf5d980359ed62c752ed98
    Fixes: Bug 1186914

--- horizon/static/horizon/js/horizon.tables.js
+++ horizon/static/horizon/js/horizon.tables.js
@@ -244,22 +244,19 @@ $(parent).find("table.datatable").each(function () {
       header_options = {};
   // Disable if not sortable or has <= 1 item
   if ($table.find('tbody tr').not('.empty').length > 1){
-    $table.find("thead th").each(function (i, val) {
+    $table.find("thead th[class!='table_header']").each(function (i, val) {
       $th = $(this);
       if (!$th.hasClass('sortable')) {
         header_options[i] = {sorter: false};
       } else if ($th.data('type') == 'size'){
-        // set as [i-1] as there is one more <th> in <thead>
-        // than <td>'s in <tbody>
-        header_options[i-1] = {sorter: 'sizeSorter'};
+        header_options[i] = {sorter: 'sizeSorter'};
       } else if ($th.data('type') == 'ip'){
-        // set as [i-1] as there is one more <th> in <thead>
-        // than <td>'s in <tbody>
-        header_options[i-1] = {sorter: 'ipAddress'};
+        header_options[i] = {sorter: 'ipAddress'};
       }
     });
     $table.tablesorter({
       headers: header_options,
+      selectorHeaders: "thead th[class!='table_header']",
       cancelSelection: false
     });
   }
