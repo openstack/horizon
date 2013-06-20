a6fcfb4e73c67a0fc2b59a610779a6871d242146
Julie Pichon <jpichon@redhat.com>
2013-06-20 16:20:07 +0100

    Clean-up some of the workflows-related javascript
     * Initialise the workflows javascript outside modals too
    
      When opening the Modify Users page into a new window rather than as
      a modal form, the "All Users" and "Project Members" lists would show
      as empty as the javascript for initialising workflow wasn't
      called. Also fix similar problems in other workflows such as Edit
      Security Groups, Launch Instances' network list.
    
     * Avoid having the projects-specific workflow js initialised on every
       modal init
    
     * Move the Network List drag & drop to horizon.instances.js as it's
       unrelated to Project workflows
    
    Also:
     * Fix a typo in help text
    
    Fixes bug #1193007
    
    Change-Id: If476d6babf3d93bd6e9abb051008aff4fb393436

--- horizon/static/horizon/js/horizon.instances.js
+++ horizon/static/horizon/js/horizon.instances.js
@@ -1,5 +1,7 @@
 horizon.instances = {
   user_decided_length: false,
+  networks_selected: [],
+  networks_available: [],
 
   getConsoleLog: function(via_user_submit) {
     var form_element = $("#tail_length"),
@@ -40,6 +42,121 @@ horizon.instances = {
         e.stopPropagation();
       }
     });
+  },
+
+  /*
+   * Gets the html select element associated with a given
+   * network id for network_id.
+   **/
+  get_network_element: function(network_id) {
+      return $('li > label[for^="id_network_' + network_id + '"]');
+  },
+
+  /*
+   * Initializes an associative array of lists of the current
+   * networks.
+   **/
+  init_network_list: function() {
+    horizon.instances.networks_selected = [];
+    horizon.instances.networks_available = [];
+    $(this.get_network_element("")).each(function(){
+      var $this = $(this);
+      var $input = $this.children("input");
+      var network_property = {
+        name:$this.text().replace(/^\s+/,""),
+        id:$input.attr("id"),
+        value:$input.attr("value")
+      };
+      if($input.is(':checked')) {
+        horizon.instances.networks_selected.push(network_property);
+      } else {
+        horizon.instances.networks_available.push(network_property);
+      }
+    });
+  },
+
+  /*
+   * Generates the HTML structure for a network that will be displayed
+   * as a list item in the network list.
+   **/
+  generate_network_element: function(name, id, value) {
+    var $li = $('<li>');
+    $li.attr('name', value).html(name + '<em class="network_id">(' + value + ')</em><a href="#" class="btn btn-primary"></a>');
+    return $li;
+  },
+
+  /*
+   * Generates the HTML structure for the Network List.
+   **/
+  generate_networklist_html: function() {
+    var self = this;
+    var updateForm = function() {
+      var lists = $("#networkListId div.input li").attr('data-index',100);
+      var active_networks = $("#selected_network > li").map(function(){
+        return $(this).attr("name");
+      });
+      $("#networkListId div.input input:checkbox").removeAttr('checked');
+      active_networks.each(function(index, value){
+        $("#networkListId div.input input:checkbox[value=" + value + "]")
+        .attr('checked','checked')
+        .parents("li").attr('data-index',index);
+      });
+      $("#networkListId div.input ul").html(
+        lists.sort(function(a,b){
+          if( $(a).data("index") < $(b).data("index")) return -1;
+          if( $(a).data("index") > $(b).data("index")) return 1;
+          return 0;
+        })
+      );
+    };
+    $("#networkListSortContainer").show();
+    $("#networkListIdContainer").hide();
+    self.init_network_list();
+    // Make sure we don't duplicate the networks in the list
+    $("#available_network").empty();
+    $.each(self.networks_available, function(index, value){
+      $("#available_network").append(self.generate_network_element(value.name, value.id, value.value));
+    });
+    // Make sure we don't duplicate the networks in the list
+    $("#selected_network").empty();
+    $.each(self.networks_selected, function(index, value){
+      $("#selected_network").append(self.generate_network_element(value.name, value.id, value.value));
+    });
+    // $(".networklist > li").click(function(){
+    //   $(this).toggleClass("ui-selected");
+    // });
+    $(".networklist > li > a.btn").click(function(e){
+      var $this = $(this);
+      e.preventDefault();
+      e.stopPropagation();
+      if($this.parents("ul#available_network").length > 0) {
+        $this.parent().appendTo($("#selected_network"));
+      } else if ($this.parents("ul#selected_network").length > 0) {
+        $this.parent().appendTo($("#available_network"));
+      }
+      updateForm();
+    });
+    if ($("#networkListId > div.control-group.error").length > 0) {
+      var errortext = $("#networkListId > div.control-group.error").find("span.help-inline").text();
+      $("#selected_network_h4").before($('<div class="dynamic-error">').html(errortext));
+    }
+    $(".networklist").sortable({
+        connectWith: "ul.networklist",
+        placeholder: "ui-state-highlight",
+        distance: 5,
+        start:function(e,info){
+          $("#selected_network").addClass("dragging");
+        },
+        stop:function(e,info){
+          $("#selected_network").removeClass("dragging");
+          updateForm();
+        }
+    }).disableSelection();
+  },
+
+  workflow_init: function(modal) {
+    // Initialise the drag and drop network list
+    horizon.instances.generate_networklist_html();
   }
 };
 
