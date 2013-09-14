9e4e8682dbe7dade0f34f254f7ddac3e3d5a27ce
Nicolas Simonds <nic@metacloud.com>
2013-09-13 17:25:02 -0700

    Store user settings in persistent cookies as well as the session
    This should keep international users from having to reset their
    language preferences every time they login.
    
    Fixes Bug: 1225250
    
    Change-Id: I093e7a98085bf83890b295ec4a61f839b60db4db

--- openstack_dashboard/settings.py
+++ openstack_dashboard/settings.py
@@ -198,6 +198,7 @@ LANGUAGES = (
     ('zh-tw', gettext_noop('Traditional Chinese')),
 )
 LANGUAGE_CODE = 'en'
+LANGUAGE_COOKIE_NAME = 'horizon_language'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
