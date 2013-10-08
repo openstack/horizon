0fa0fd006662420b8f77ad0c6bd909126fccbaab
Yves-Gwenael Bourhis <yves-gwenael.bourhis@cloudwatt.com>
2013-10-08 18:53:25 +0200

    Logging user out after self password change
    Admin users where not logged out after changing their own password in the user
    settings pannel, and error messages where displayed afterwards.
    
    Fixes bug 1226829
    
    Change-Id: Iec0e34484e2bbc28a300d6259aab1a682e4a00ff
    (cherry-picked from commit 0aab590)

--- horizon/utils/functions.py
+++ horizon/utils/functions.py
@@ -1,5 +1,8 @@
 import math
 
+from django.conf import settings  # noqa
+from django.contrib.auth import logout  # noqa
+from django import http
 from django.utils.encoding import force_unicode  # noqa
 from django.utils.functional import lazy  # noqa
 from django.utils import translation
@@ -24,3 +27,16 @@ def add_logout_reason(request, response, reason):
     with translation.override(lang):
         reason = unicode(reason).encode('utf-8')
         response.set_cookie('logout_reason', reason, max_age=30)
+
+
+def logout_with_message(request, msg):
+    """Send HttpResponseRedirect to LOGOUT_URL.
+
+    `msg` is a message displayed on the login page after the logout, to explain
+    the logout reson.
+    """
+    logout(request)
+    response = http.HttpResponseRedirect(
+        '%s?next=%s' % (settings.LOGOUT_URL, request.path))
+    add_logout_reason(request, response, msg)
+    return response
