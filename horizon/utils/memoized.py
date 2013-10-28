755509c169fdba2032c838697a874bd561835780
Radomir Dopieralski <openstack@sheep.art.pl>
2013-10-28 13:56:54 +0100

    Better @memoized decorator
    This version of the @memoized decorator only keeps weak references to
    the function arguments in cache, which means that it doesn't keep
    objects alive forever, and that it won't fill up all available memory
    eventually, as the old version would -- it only keeps cache for the
    objects that are actually in use.
    
    Honestly, I'm not actually sure that the effect is worth it, as the
    level of complexity of this code is quite high. I thought I would
    submit it anyways, so that we can at least have a discussion.
    
    Change-Id: I3a4fca3aba7c52102673f7fa5d127873c71516f8
    Partial-Bug: #1098047

--- horizon/utils/memoized.py
+++ horizon/utils/memoized.py
@@ -15,36 +15,80 @@
 #    under the License.
 
 import functools
+import weakref
 
 
-class memoized(object):
-    '''Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    '''
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
+def _try_weakref(arg, remove_callback):
+    """Return a weak reference to arg if possible, or arg itself if not."""
+    try:
+        arg = weakref.ref(arg, remove_callback)
+    except TypeError:
+        # Not all types can have a weakref. That includes strings
+        # and floats and such, so just pass them through directly.
+        pass
+    return arg
 
-    def __call__(self, *args):
+
+def _get_key(args, kwargs, remove_callback):
+    """Calculate the cache key, using weak references where possible."""
+    # Use tuples, because lists are not hashable.
+    weak_args = tuple(_try_weakref(arg, remove_callback) for arg in args)
+    # Use a tuple of (key, values) pairs, because dict is not hashable.
+    # Sort it, so that we don't depend on the order of keys.
+    weak_kwargs = tuple(sorted(
+        (key, _try_weakref(value, remove_callback))
+        for (key, value) in kwargs.iteritems()))
+    return weak_args, weak_kwargs
+
+
+def memoized(func):
+    """Decorator that caches function calls.
+
+    Caches the decorated function's return value the first time it is called
+    with the given arguments.  If called later with the same arguments, the
+    cached value is returned instead of calling the decorated function again.
+
+    The cache uses weak references to the passed arguments, so it doesn't keep
+    them alive in memory forever.
+    """
+    # The dictionary in which all the data will be cached. This is a separate
+    # instance for every decorated function, and it's stored in a closure of
+    # the wrapped function.
+    cache = {}
+
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        # We need to have defined key early, to be able to use it in the
+        # remove() function, but we calculate the actual value of the key
+        # later on, because we need the remove() function for that.
+        key = None
+
+        def remove(ref):
+            """A callback to remove outdated items from cache."""
+            try:
+                # The key here is from closure, and is calculated later.
+                del cache[key]
+            except KeyError:
+                # Some other weak reference might have already removed that
+                # key -- in that case we don't need to do anything.
+                pass
+
+        key = _get_key(args, kwargs, remove)
         try:
-            return self.cache[args]
+            # We want cache hit to be as fast as possible, and don't really
+            # care much about the speed of a cache miss, because it will only
+            # happen once and likely calls some external API, database, or
+            # some other slow thing. That's why the hit is in straightforward
+            # code, and the miss is in an exception.
+            value = cache[key]
         except KeyError:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
+            value = cache[key] = func(*args, **kwargs)
         except TypeError:
-            # uncachable -- for instance, passing a list as an argument.
-            # Better to not cache than to blow up entirely.
-            return self.func(*args)
+            # The calculated key may be unhashable when an unhashable object,
+            # such as a list, is passed as one of the arguments. In that case,
+            # we can't cache anything and simply always call the decorated
+            # function.
+            value = func(*args, **kwargs)
+        return value
 
-    def __repr__(self):
-        '''Return the function's docstring.'''
-        return self.func.__doc__ or ''
-
-    def __get__(self, obj, objtype):
-        '''Support instance methods.'''
-        return functools.partial(self.__call__, obj)
-
-    def __str__(self):
-        return str(self.func)
+    return wrapped
