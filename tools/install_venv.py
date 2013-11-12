7a0581bb701dbb4d7d9fb1bbe1d7644e98ac2b2e
Akihiro MOTOKI <motoki@da.jp.nec.com>
2013-11-13 03:30:42 +0900

    Import install_venv from oslo
    The new version of install_venv_common evaluates requirements.txt
    and test-requirements.txt at the same time and we avoid Django 1.6
    is installed when evaluating django-nose dependencies.
    
    Change-Id: I2ab7108b3615ac6c0525089b81b80e5c0a4793fc
    Closes-Bug: #1250581

--- tools/install_venv.py
+++ tools/install_venv.py
@@ -6,45 +6,45 @@
 #
 # Copyright 2010 OpenStack Foundation
 # Copyright 2013 IBM Corp.
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
-import ConfigParser
 import os
 import sys
 
-import install_venv_common as install_venv  # flake8: noqa
+import install_venv_common as install_venv  # noqa
 
 
-def print_help(project, venv, root):
+def print_help(venv, root):
     help = """
-    %(project)s development environment setup is complete.
+    Openstack development environment setup is complete.
 
-    %(project)s development uses virtualenv to track and manage Python
+    Openstack development uses virtualenv to track and manage Python
     dependencies while in development and testing.
 
-    To activate the %(project)s virtualenv for the extent of your current
-    shell session you can run:
+    To activate the Openstack virtualenv for the extent of your current shell
+    session you can run:
 
-    $ source %(venv)s/bin/activate
+    $ source %s/bin/activate
 
-    Or, if you prefer, you can run commands in the virtualenv on a case by
-    case basis by running:
+    Or, if you prefer, you can run commands in the virtualenv on a case by case
+    basis by running:
 
-    $ %(root)s/tools/with_venv.sh <your command>
+    $ %s/tools/with_venv.sh <your command>
+
+    Also, make test will automatically use the virtualenv.
     """
-    print help % dict(project=project, venv=venv, root=root)
+    print(help % (venv, root))
 
 
 def main(argv):
@@ -59,19 +59,15 @@ def main(argv):
     pip_requires = os.path.join(root, 'requirements.txt')
     test_requires = os.path.join(root, 'test-requirements.txt')
     py_version = "python%s.%s" % (sys.version_info[0], sys.version_info[1])
-    setup_cfg = ConfigParser.ConfigParser()
-    setup_cfg.read('setup.cfg')
-    project = setup_cfg.get('metadata', 'name')
-
-    install = install_venv.InstallVenv(
-        root, venv, pip_requires, test_requires, py_version, project)
+    project = 'Openstack'
+    install = install_venv.InstallVenv(root, venv, pip_requires, test_requires,
+                                       py_version, project)
     options = install.parse_args(argv)
     install.check_python_version()
     install.check_dependencies()
     install.create_virtualenv(no_site_packages=options.no_site_packages)
     install.install_dependencies()
-    install.post_process()
-    print_help(project, venv, root)
+    print_help(venv, root)
 
 if __name__ == '__main__':
     main(sys.argv)
