77c35574e52a0653aa75ca26b1ec2ab38eaebff8
liyingjun <liyingjun1988@gmail.com>
2013-11-22 23:34:27 +0800

    Add missing egress sec group for all tcp/udp/icmp
    When creating "Security Group Egress ALL TCP/UDP/ICMP", the
    direction is always Ingress, need to add egress in the situation.
    
    Fixes bug #1254040
    
    Change-Id: I1a2bdc07d7ca4ecc12ebdb35a1f357de0d09c502

--- openstack_dashboard/test/test_data/neutron_data.py
+++ openstack_dashboard/test/test_data/neutron_data.py
@@ -392,10 +392,18 @@ def data(TEST):
             'remote_ip_prefix': None,
             'security_group_id': secgroup['id'],
             'tenant_id': secgroup['tenant_id']}
+        rule_all_tcp = {
+            'id': str(uuid.uuid4()),
+            'direction': u'egress', 'ethertype': u'IPv4',
+            'port_range_min': 1, 'port_range_max': 65535,
+            'protocol': u'tcp', 'remote_group_id': None,
+            'remote_ip_prefix': u'0.0.0.0/24',
+            'security_group_id': secgroup['id'],
+            'tenant_id': secgroup['tenant_id']}
 
         rules = []
         if not default_only:
-            rules += [rule_tcp_80, rule_icmp, rule_group]
+            rules += [rule_tcp_80, rule_icmp, rule_group, rule_all_tcp]
         rules += [rule_egress_ipv4, rule_egress_ipv6]
         secgroup['security_group_rules'] = rules
 
