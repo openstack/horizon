135631529ae6bff12635a5d9800134af36d4eb88
Maxime Vidori <maxime.vidori@enovance.com>
2014-02-06 20:32:18 +0100

    Replace horizon.utils with an angular one
        Horizon utilitaries are now fully integrated into the angular
    application. The object horizon.utils is still available in the
    application in order to keep the compatibility, it is a link to the
    angular constant object.
    
    Change-Id: I3d44c5c281a8e61e93f4d1b7c3b8b61c128e36ea
    Implements: blueprint horizon-angular

---
+++ horizon/static/horizon/js/angular/services/horizon.utils.js
@@ -0,0 +1,64 @@
+/*global angular*/
+(function () {
+  'use strict';
+  function utils(hzConfig, $log, $rootScope, $compile) {
+    return {
+      /*
+       Use the log levels of http://docs.angularjs.org/api/ng.$log
+       default to log level.
+       */
+      log: function (msg, lvl) {
+        if (hzConfig.debug) {
+          ($log[lvl] || $log.log)(msg);
+        }
+      },
+      capitalize: function (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      },
+      /*
+       Adds commas to any integer or numbers within a string for human display.
+
+       EG:
+       horizon.utils.humanizeNumbers(1234); -> "1,234"
+       horizon.utils.humanizeNumbers("My Total: 1234"); -> "My Total: 1,234"
+       */
+      humanizeNumbers: function (number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      },
+
+      /*
+       Truncate a string at the desired length. Optionally append an ellipsis
+       to the end of the string.
+
+       EG:
+       horizon.utils.truncate("String that is too long.", 18, true); ->
+       "String that is too&hellip;"
+       */
+      truncate: function (string, size, includeEllipsis) {
+        if (string.length > size) {
+          if (includeEllipsis) {
+            return string.substring(0, (size - 3)) + "&hellip;";
+          }
+
+          return string.substring(0, size);
+        }
+
+        return string;
+      },
+      loadAngular: function (element) {
+        try {
+          $compile(element)($rootScope);
+          $rootScope.$apply();
+        } catch (err) {}
+        /*
+         Compilation fails when it could not find a directive,
+         fails silently on this, it is an angular behaviour.
+         */
+
+      }
+    };
+  }
+
+  angular.module('hz.utils', ['hz.conf'])
+    .service('hzUtils', ['hzConfig', '$log', '$rootScope', '$compile', utils]);
+}());
