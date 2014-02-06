2d591beff5a35aaa13b11e41705ed7a1d5358502
Maxime Vidori <maxime.vidori@enovance.com>
2014-02-06 18:58:33 +0100

    Replace horizon.conf with an angular one
        Horizon configuration is now fully integrated into the angular
    application. The object horizon.conf is still available in the
    application in order to keep the compatibility, it is a link to the
    angular constant object.
    
    Change-Id: I55e1672a89a78032b24c096258471d0ea9d2489e
    Implements: blueprint horizon-angular

--- horizon/static/horizon/js/horizon.utils.js
+++ horizon/static/horizon/js/horizon.utils.js
@@ -43,7 +43,7 @@ horizon.utils = {
   },
 
   loadAngular: function (element) {
-    angular.injector(['ng', 'horizonApp']).
+    angular.injector(['ng', 'hz']).
       invoke(['$rootScope', '$compile', function ($rootScope, $compile) {
         try {
           $compile(element)($rootScope);
