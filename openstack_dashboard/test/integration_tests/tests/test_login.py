5001bca9753292497075dc0b0ec73167f60e672a
Daniel Korn <dkorn@redhat.com>
2014-03-02 12:43:01 +0200

    Integration tests - page objects pattern
    First pages:
    * pageobject - the base page for all page objects
    * loginpage - the login page object
    * basepage - base class for all dashboard page objects
    * adminpage
    * projectpage
    
    First test:
    test_login - a basic scenario test:
    * checks that the login page is available
    * logs in as a regular user
    * checks that the user home page loads without error
    
    https://wiki.openstack.org/wiki/Horizon/Testing/UI
    
    Partially implements blueprint: selenium-integration-testing
    
    Change-Id: Icad82f5f7810c348ddc30cd767f1d3bf5ecb926e

--- openstack_dashboard/test/integration_tests/tests/test_login.py
+++ openstack_dashboard/test/integration_tests/tests/test_login.py
@@ -1,43 +1,29 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
-import selenium.webdriver.common.keys as keys
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 from openstack_dashboard.test.integration_tests import helpers
+from openstack_dashboard.test.integration_tests.pages import loginpage
 
 
-class TestLoginPage(helpers.BaseTestCase):
+class TestLogin(helpers.BaseTestCase):
     """This is a basic scenario test:
     * checks that the login page is available
     * logs in as a regular user
     * checks that the user home page loads without error
-
-    FIXME(jpichon): This test will be rewritten using the Page Objects
-    pattern, which is much more maintainable.
-
     """
-
     def test_login(self):
-        self.driver.get(self.conf.dashboard.login_url)
-        self.assertIn("Login", self.driver.title)
-
-        username = self.driver.find_element_by_name("username")
-        password = self.driver.find_element_by_name("password")
-
-        username.send_keys(self.conf.identity.username)
-        password.send_keys(self.conf.identity.password)
-        username.send_keys(keys.Keys.RETURN)
-
-        self.wait_for_title()
-        self.assertIn("Instance Overview", self.driver.title)
+        login_pg = loginpage.LoginPage(self.driver, self.conf)
+        login_pg.go_to_login_page()
+        home_pg = login_pg.login()
+        if not home_pg.is_logged_in:
+            self.fail("Could not determine if logged in")
+        home_pg.log_out()
