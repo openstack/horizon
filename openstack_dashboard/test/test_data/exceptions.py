937436f95c9311db9f907051e56a609040f572b0
Matthias Runge <mrunge@redhat.com>
2014-07-11 12:45:19 +0200

    adapt to python-novaclient-2.18.0
    Novaclient renamed HTTPNotImplemented to HttpNotImplemented
    and changed the attribute code to status in exceptions
    
    This change should make the code work on newer and older versions.
    
    Closes-Bug: #1340596
    (cherry picked from commit 322004f8480e40af5e77ed843ff803005d5af27c)
    
    Conflicts:
    	openstack_dashboard/dashboards/project/instances/console.py
    	openstack_dashboard/test/test_data/exceptions.py
    
    Change-Id: Ifef6c4c1b2924b03df00f427cfe3e6a7f415e569

--- openstack_dashboard/test/test_data/exceptions.py
+++ openstack_dashboard/test/test_data/exceptions.py
@@ -28,7 +28,11 @@ def create_stubbed_exception(cls, status_code=500):
     msg = "Expected failure."
 
     def fake_init_exception(self, code, message, **kwargs):
-        self.code = code
+        if code is not None:
+            if hasattr(self, 'http_status'):
+                self.http_status = code
+            else:
+                self.code = code
         self.message = message
 
     def fake_str(self):
