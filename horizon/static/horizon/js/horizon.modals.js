8f808a69f6f64c7d92571989b72da1d1801a1c86
Timur Sufiev <tsufiev@mirantis.com>
2014-08-01 19:29:16 +0400

    Show validation errors in modal forms
    Selector '.modal span.help-block' is the exact one for the
    validation messages. It might have been used for some
    other purpose previously, but now it breaks the things.
    
    Change-Id: Ibb55f06533a21fc425a6bdac9b71b7e489223dd1
    Closes-Bug: #1351298

--- horizon/static/horizon/js/horizon.modals.js
+++ horizon/static/horizon/js/horizon.modals.js
@@ -41,7 +41,6 @@ horizon.modals.create = function (title, body, confirm, cancel) {
 horizon.modals.success = function (data, textStatus, jqXHR) {
   var modal;
   $('#modal_wrapper').append(data);
-  $('.modal span.help-block').hide();
   modal = $('.modal:last');
   modal.modal();
   $(modal).trigger("new_modal", modal);
