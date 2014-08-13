7dd2aa8b4a3126145888b45d6f081d8a38454c0f
Thai Tran <tqtran@us.ibm.com>
2014-08-12 20:12:13 -0700

    Context selection for project and region
    Providing the mechanism to easily switch between projects and regions.
    
    Only shows domain info if OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT is set
    to True in settings.
    
    Will add domain switching for v3 once support for it is complete.
    Will add advance filtering rules in follow up patch.
    
    Co-Authored-By: David Lyle <david.lyle@hp.com>
    
    Implements: blueprint context-selection
    
    Change-Id: Ie6e81eebe822ab2394b07345d148a1c06a33a25f

---
+++ openstack_dashboard/templatetags/context_selection.py
@@ -0,0 +1,86 @@
+# Copyright 2014 IBM Corp.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import absolute_import
+
+from django.conf import settings
+from django import template
+
+from openstack_dashboard.api import keystone
+
+
+register = template.Library()
+
+
+def is_multidomain_supported():
+    return (keystone.VERSIONS.active >= 3 and
+            getattr(settings,
+                    'OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT',
+                    False))
+
+
+@register.inclusion_tag('context_selection/_overview.html',
+                        takes_context=True)
+def show_overview(context):
+    if 'request' not in context:
+        return {}
+    request = context['request']
+    context = {'domain_supported': is_multidomain_supported(),
+               'domain_name': request.user.user_domain_id,
+               'project_name': request.user.project_name,
+               'multi_region':
+                    len(request.user.available_services_regions) > 1,
+               'region_name': request.user.services_region,
+               'request': request}
+
+    return context
+
+
+@register.inclusion_tag('context_selection/_domain_list.html',
+                        takes_context=True)
+def show_domain_list(context):
+    # TODO(Thai): once domain switching is support, need to revisit
+    if 'request' not in context:
+        return {}
+    request = context['request']
+    context = {'domain_supported': is_multidomain_supported(),
+               'domain_name': request.user.user_domain_id,
+               'request': request}
+    return context
+
+
+@register.inclusion_tag('context_selection/_project_list.html',
+                        takes_context=True)
+def show_project_list(context):
+    if 'request' not in context:
+        return {}
+    request = context['request']
+    context = {'projects': context['authorized_tenants'],
+               'project_id': request.user.project_id,
+               'request': request}
+    return context
+
+
+@register.inclusion_tag('context_selection/_region_list.html',
+                        takes_context=True)
+def show_region_list(context):
+    if 'request' not in context:
+        return {}
+    request = context['request']
+    context = {'multi_region':
+                    len(request.user.available_services_regions) > 1,
+               'region_name': request.user.services_region,
+               'regions': request.user.available_services_regions,
+               'request': request}
+    return context
