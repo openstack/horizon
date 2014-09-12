d9870c7e851bcfa7f91652c636c117539c8bf63b
Andreas Jaeger <aj@suse.de>
2014-09-12 22:38:24 +0200

    Stop using intersphinx
    Remove intersphinx from the docs build as it triggers network calls that
    occasionally fail, and we don't really use intersphinx (links other
    sphinx documents out on the internet)
    
    This also removes the requirement for internet access during docs build.
    
    This causes docs jobs to fail because we error out on warnings.
    
    Change-Id: I71e941e2a639641a662a163c682eb86d51de42fb
    Related-Bug: #1368910

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -153,7 +153,6 @@ write_autodoc_index()
 # They can be extensions coming with Sphinx (named 'sphinx.ext.*')
 # or your custom ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
               'sphinx.ext.coverage',
               'sphinx.ext.pngmath',
@@ -426,13 +425,3 @@ epub_copyright = u'2012, OpenStack'
 
 # Allow duplicate toc entries.
 # epub_tocdup = True
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('http://docs.python.org/', None),
-                       'django': (
-                           'http://docs.djangoproject.com/en/dev/_objects/'),
-                       'nova': ('http://nova.openstack.org', None),
-                       'swift': ('http://swift.openstack.org', None),
-                       'keystone': ('http://keystone.openstack.org', None),
-                       'glance': ('http://glance.openstack.org', None)}
