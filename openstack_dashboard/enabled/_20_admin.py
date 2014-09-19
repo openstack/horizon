2ad5e226371838306a6a586acb84313e39473a66
Enrique Garcia Navalon <garcianavalon@gmail.com>
2014-09-19 14:34:26 +0200

    disabled nonused modules

--- openstack_dashboard/enabled/_20_admin.py
+++ openstack_dashboard/enabled/_20_admin.py
@@ -1,6 +1,6 @@
 # The name of the dashboard to be added to HORIZON['dashboards']. Required.
 DASHBOARD = 'admin'
-
+DISABLED = True
 # A list of applications to be added to INSTALLED_APPS.
 ADD_INSTALLED_APPS = [
     'openstack_dashboard.dashboards.admin',
--- openstack_dashboard/local/enabled/_50_idm.py
+++ openstack_dashboard/local/enabled/_50_idm.py
@@ -3,7 +3,7 @@ DASHBOARD = 'idm'
 
 # If set to True, this dashboard will not be added to the settings.
 DISABLED = False
-
+DEFAULT = True
 # A list of applications to be added to INSTALLED_APPS.
 ADD_INSTALLED_APPS = [
     'openstack_dashboard.dashboards.idm',
