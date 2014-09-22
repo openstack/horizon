d77d8dc143ac5a1e6e5d4f842be97e6767034342
Enrique Garcia Navalon <garcianavalon@gmail.com>
2014-09-22 17:36:17 +0200

    account creationg working for v2 API. No project is set for now so no login is possible

--- openstack_dashboard/fiware_auth/views.py
+++ openstack_dashboard/fiware_auth/views.py
@@ -25,16 +25,22 @@ from django.views.decorators.debug import sensitive_post_parameters # noqa
 from keystoneclient import exceptions as keystone_exceptions
 from keystoneclient.v2_0 import client as keystone_client_v2
 
-from openstack_auth import utils
 from django.shortcuts import redirect
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 from forms import RegistrationForm
 
-try:
-	is_safe_url = http.is_safe_url
-except AttributeError:
-	is_safe_url = utils.is_safe_url
+from django.forms import ValidationError  # noqa
+from django import http
+from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.debug import sensitive_variables  # noqa
+
+from horizon import exceptions
+from horizon import forms
+from horizon import messages
+from horizon.utils import validators
+
+from openstack_dashboard import api
 
 LOG = logging.getLogger(__name__)
 
@@ -45,9 +51,25 @@ class RegistrationView(FormView):
 	"""
 	form_class = RegistrationForm
 	http_method_names = ['get', 'post', 'head', 'options', 'trace']
-	success_url = None
-	template_name = 'registration/registration_form.html'
+	success_url = '/' #TODO'
+	template_name = 'auth/registration_form.html'
 
+	def post(self, request, *args, **kwargs):
+		# Pass request to get_form_class and get_form for per-request
+		# form control.
+		form_class = self.get_form_class(request)
+		form = self.get_form(form_class)
+		if form.is_valid():
+			# Pass request to form_valid.
+			return self.form_valid(request, form)
+		else:
+			return self.form_invalid(form)
+	def get_form_class(self, request=None):
+		return super(RegistrationView, self).get_form_class()
+ 	def get_success_url(self, request=None, user=None):
+		# We need to be able to use the request and the new user when
+		# constructing success_url.
+		return super(RegistrationView, self).get_success_url()				
 	def form_valid(self, request, form):
 		new_user = self.register(request, **form.cleaned_data)
 		success_url = self.get_success_url(request, new_user)
@@ -62,24 +84,29 @@ class RegistrationView(FormView):
 		except ValueError:
 		    return redirect(success_url)
 
+	# We have to protect the entire "cleaned_data" dict because it contains the
+    # password and confirm_password strings.
+	
 	def register(self, request, **cleaned_data):
-
 		msg = 'Singup user "%(username)s".' % {'username': request.user.username}
 		LOG.info(msg)
-		#Load the form data
-		username, email, password = cleaned_data['username'], cleaned_data['email'], cleaned_data['password1']
-		#Load keystone stuff
-		endpoint = request.session.get('region_endpoint')
-		token = request.session.get('token')
 		#create the user
-		#User.objects.create_user(username, email, password)
-		new_user = authenticate(username=username, password=password)
-		#login(request, new_user)
-
-		signals.user_registered.send(sender=self.__class__,
-		                             user=new_user,
-		                             request=request)
-		return new_user
-
+		domain = api.keystone.get_default_domain(self.request)
+		try:
+			LOG.info('Creating user with name "%s"' % cleaned_data['username'])
+			if "email" in cleaned_data:
+				cleaned_data['email'] = cleaned_data['email'] or None
+			new_user = api.keystone.user_create(request,
+												name=cleaned_data['username'],
+												email=cleaned_data['email'],
+												password=cleaned_data['password1'],
+												project=None,
+												enabled=True,
+												domain=domain.id)
+			messages.success(request,
+				_('User "%s" was successfully created.') % cleaned_data['username'])
+			return new_user
+		except Exception:
+			exceptions.handle(request, _('Unable to create user.'))
                 
 
--- openstack_dashboard/urls.py
+++ openstack_dashboard/urls.py
@@ -33,7 +33,8 @@ import horizon
 urlpatterns = patterns('',
     url(r'^$', 'openstack_dashboard.views.splash', name='splash'),
     url(r'^auth/', include('openstack_auth.urls')),
-    url(r'', include(horizon.urls))
+    url(r'', include(horizon.urls)),
+    url(r'',include('fiware_auth.urls'))
 )
 
 # Development static app and project media serving using the staticfiles app.
