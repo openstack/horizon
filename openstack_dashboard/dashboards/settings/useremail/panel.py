8a87434f794ce3789fd19cfdc16f0fef49be643c
Enrique Garcia Navalon <garcianavalon@gmail.com>
2014-09-24 12:02:52 +0200

    basic panel for email settings

---
+++ openstack_dashboard/dashboards/settings/useremail/panel.py
@@ -0,0 +1,13 @@
+from django.utils.translation import ugettext_lazy as _
+
+import horizon
+
+from openstack_dashboard.dashboards.settings import dashboard
+
+
+class Useremail(horizon.Panel):
+    name = _("Useremail")
+    slug = "useremail"
+
+
+dashboard.Settings.register(Useremail)
