9368eaec6a4462f5491734f7ae5fecf7ec5c22a2
Enrique Garcia Navalon <garcianavalon@gmail.com>
2014-10-06 17:15:09 +0200

    populate form field with current user email

--- openstack_dashboard/dashboards/settings/useremail/views.py
+++ openstack_dashboard/dashboards/settings/useremail/views.py
@@ -1,6 +1,6 @@
 
 from horizon import forms
-#from openstack_dashboard import api
+from openstack_dashboard import api
 from openstack_dashboard.dashboards.settings.useremail import forms as useremail_forms
 
 
@@ -10,8 +10,8 @@ class EmailView(forms.ModalFormView):
 
     def get_form_kwargs(self):
         kwargs = super(EmailView, self).get_form_kwargs()
-        #user_id=self.request.user.id
-        #user = api.keystone.user_get(self.request,user_id,admin=False)
-        #kwargs['email'] = user['email']
+        user_id=self.request.user.id
+        user = api.keystone.user_get(self.request,user_id,admin=False)
+        kwargs['email'] = user['email']
         return kwargs
 
