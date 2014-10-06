65916240223e45f34e64a7e573b264a2ecc40975
sorube13 <sorube@gmail.com>
2014-10-06 10:22:30 +0200

    applications deleted

--- openstack_dashboard/dashboards/idm/applications/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns
-from django.conf.urls import url
-
-from openstack_dashboard.dashboards.idm.applications import views
-
-
-urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-)
