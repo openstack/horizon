b5fe528bdd96564544a786897380f40d7dbed9c6
sorube13 <sorube@gmail.com>
2014-10-08 12:07:59 +0200

    views to create new application

--- openstack_dashboard/dashboards/idm/myApplications/forms.py
+++ openstack_dashboard/dashboards/idm/myApplications/forms.py
@@ -13,22 +13,26 @@ from openstack_auth import exceptions as auth_exceptions
 
 
 class CreateApplicationForm(forms.SelfHandlingForm):
-	name = forms.CharField(label=_("Name"), required=True)
-	description = forms.CharField(label=_("Description"),widget=forms.Textarea, required=True)
-	url = forms.CharField(label=_("URL"), required=True)
-	callbackurl = forms.CharField(label=_("Callback URL"), required=True)
+	name = forms.CharField(label=_("Name"), required=False)
+	description = forms.CharField(label=_("Description"),widget=forms.Textarea, required=False)
+	url = forms.CharField(label=_("URL"), required=False)
+	callbackurl = forms.CharField(label=_("Callback URL"), required=False)
 
-	def handle(self, request, data):
-		# user_id=request.user.id
-		# user = api.keystone.user_get(request,user_id,admin=False)    
-		response = shortcuts.redirect(horizon.get_user_home(request.user))
-		return response
+	def handle(self, request):
+		return True
+	
 	
 class UploadImageForm(forms.Form):
-	file = forms.ImageField(required=True)
+	file = forms.ImageField(required=False)
+
+	def handle(self, request):
+		return True
+	
+
+class RolesApplicationForm(forms.SelfHandlingForm):
+	name = forms.CharField(label=_("Name"), required=False)
+	# roles = forms.ChoiceField( 'Provider', 'Purchaser')
 
 	def handle(self, request, data):
-		# user_id=request.user.id
-		# user = api.keystone.user_get(request,user_id,admin=False)    
-		response = shortcuts.redirect(horizon.get_user_home(request.user))
-		return response
+		return True
+	
