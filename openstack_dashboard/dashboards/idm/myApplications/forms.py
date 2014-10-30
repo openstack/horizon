a07ede227200b30f6f4cd8329c1da77439463ee5
sorube13 <sorube@gmail.com>
2014-10-30 13:01:21 +0100

    png works jcrop

--- openstack_dashboard/dashboards/idm/myApplications/forms.py
+++ openstack_dashboard/dashboards/idm/myApplications/forms.py
@@ -36,10 +36,7 @@ class UploadImageForm(forms.SelfHandlingForm):
 	y2 = forms.DecimalField(widget=forms.HiddenInput(),required=False)
 		
 	def handle(self, request, data):
-		x1=self.cleaned_data['x1'] 
-		x2=self.cleaned_data['x2']
-		y1=self.cleaned_data['y1']
-		y2=self.cleaned_data['y2']
+		
 				
 		
 		image = request.FILES['image'] 
@@ -47,24 +44,36 @@ class UploadImageForm(forms.SelfHandlingForm):
 		
 		img = Image.open(image)
 
-		if x1 > x2:
-			left = x2
-			right = x1
-		else:
-			left = x1
-			right = x2
-		if y1 > y2:
-			bottom = y2
-			top = y1
-		else:
-			bottom = y1
-			top = y2
+		# if x1 > x2:
+		# 	left = x2
+		# 	right = x1
+		# else:
+		# 	left = x1
+		# 	right = x2
+		# if y1 > y2:
+		# 	bottom = y2
+		# 	top = y1
+		# else:
+		# 	bottom = y1
+		# 	top = y2
+
+		# import pdb
+		# pdb.set_trace()
+
+		x1=self.cleaned_data['x1'] 
+		x2=self.cleaned_data['x2']
+		y1=self.cleaned_data['y1']
+		y2=self.cleaned_data['y2']
+
+		x1 = int(x1)
+		x2 = int(x2)
+		y1 = int(y1)
+		y2 = int(y2)
 
 		output_img=img.crop((x1,y1,x2,y2))
 		output_img.save(settings.MEDIA_ROOT+"/"+"ApplicationAvatar/"+imageName)
 
-		# import pdb
-		# pdb.set_trace()
+		
 		
 
 		response = shortcuts.redirect('horizon:idm:myApplications:roles')
