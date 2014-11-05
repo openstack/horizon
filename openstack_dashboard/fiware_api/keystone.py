ce89d53c4c7a26d882b65f4fb6722816516f7daf
Enrique Garcia Navalon <garcianavalon@gmail.com>
2014-11-05 14:42:51 +0100

    roles CRUD api

--- openstack_dashboard/fiware_api/keystone.py
+++ openstack_dashboard/fiware_api/keystone.py
@@ -16,6 +16,8 @@ from django.conf import settings
 from keystoneclient.v3 import client
 from keystoneclient import exceptions as keystoneclient_exceptions    
 
+from openstack_dashboard import api
+
 CREDENTIALS = getattr(settings, 'OPENSTACK_KEYSTONE_ADMIN_CREDENTIALS')
 CREDENTIALS['AUTH_URL'] = getattr(settings,'OPENSTACK_KEYSTONE_URL')
 
@@ -26,6 +28,7 @@ def fiware_keystoneclient():
     operations like create users, projects, etc. when there is no user with admin rights
     (for example, when user registration) to overcome the Keystone limitations.
     """
+    # TODO(garcianavalon) find a way to integrate this with the existng keystone api
     # TODO(garcianavalon)caching and efficiency
     keystone = client.Client(username=CREDENTIALS['USERNAME'], 
                                 password=CREDENTIALS['PASSWORD'], 
@@ -33,7 +36,7 @@ def fiware_keystoneclient():
                                 auth_url=CREDENTIALS['AUTH_URL'])
     return keystone
 
-def _find_user(keystone,email=None,name=None):
+def _find_user(keystone, email=None, name=None):
     # NOTE(garcianavalon) I dont know why but find by email returns a NoUniqueMatch 
     # exception so we do it by hand filtering the python dictionary, 
     # which is extremely inneficient...
@@ -51,10 +54,10 @@ def _find_user(keystone,email=None,name=None):
 
 def _grant_role(keystone, role, user, project):
     role = keystone.roles.find(name=role)
-    keystone.roles.grant(role,user=user,project=project)
+    keystone.roles.grant(role, user=user, project=project)
     return role
 
-def register_user(name,email,password):
+def register_user(name, email, password):
     keystone = fiware_keystoneclient()
     default_domain = keystone.domains.get(CREDENTIALS['DOMAIN'])
     default_project = keystone.projects.create(name, domain=default_domain)
@@ -71,22 +74,43 @@ def activate_user(user_id):
     keystone = fiware_keystoneclient()
     #default_domain = _get_default_domain(keystone)
     user = keystone.users.get(user_id)
-    project = keystone.projects.update(user.default_project_id,enabled=True)
-    user = keystone.users.update(user,enabled=True)
+    project = keystone.projects.update(user.default_project_id, enabled=True)
+    user = keystone.users.update(user, enabled=True)
     return user
 
 def change_password(user_email,new_password):
     keystone = fiware_keystoneclient()
-    user = _find_user(keystone,email=user_email)
-    user = keystone.users.update(user,password=new_password,enabled=True)
+    user = _find_user(keystone, email=user_email)
+    user = keystone.users.update(user, password=new_password, enabled=True)
     return user
 
 def check_user(name):
     keystone = fiware_keystoneclient()
-    user = _find_user(keystone,name=name)
+    user = _find_user(keystone, name=name)
     return user
 
 def check_email(email):
     keystone = fiware_keystoneclient()
-    user = _find_user(keystone,email=email)
+    user = _find_user(keystone, email=email)
     return user
+
+# ROLES AND PERMISSIONS
+def create_role(request, name, is_editable=True, application=None, **kwargs):
+    manager = api.keystone.keystoneclient(request, admin=True).fiware_roles
+    return manager.create(name=name,
+                is_editable=is_editable,
+                application=application,
+                **kwargs)
+
+def update_role(request, role, name=None, is_editable=True, 
+                application=None, **kwargs):
+    manager = api.keystone.keystoneclient(request, admin=True).fiware_roles
+    return manager.update(role, 
+                        name=name,
+                        is_editable=is_editable,
+                        application=application,
+                        **kwargs)
+        
+def delete_role(request, role):
+    manager = api.keystone.keystoneclient(request, admin=True).fiware_roles
+    return manager.delete(role)
