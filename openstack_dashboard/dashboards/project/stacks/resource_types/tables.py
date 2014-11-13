36273e917b042e9840f93ececf5042a7e9957fe2
Tatiana Ovchinnikova <t.v.ovtchinnikova@gmail.com>
2014-11-13 19:12:30 +0300

    Orchestration Resource types panel
    This patch set adds 'Resources' panel to 'Orchestration' to
    provide user with a list of all available resource types.
    
    Partially implements blueprint: heat-ui-improvement
    
    Change-Id: I0e26a1009500cc4fa78ef27ca06553cdc987fd48

---
+++ openstack_dashboard/dashboards/project/stacks/resource_types/tables.py
@@ -0,0 +1,44 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.utils.translation import ugettext_lazy as _
+
+from horizon import tables
+
+
+class ResourceTypesTable(tables.DataTable):
+    class ResourceColumn(tables.Column):
+        def get_raw_data(self, datum):
+            attr_list = ['implementation', 'component', 'resource']
+            info_list = datum.resource_type.split('::')
+            info_list[0] = info_list[0].replace("AWS", "Amazon Web Services").\
+                replace("OS", "OpenStack")
+            info_dict = dict(zip(attr_list, info_list))
+            return info_dict[self.transform]
+
+    name = tables.Column("resource_type",
+                         verbose_name=_("Type"),
+                         link="horizon:project:stacks.resource_types:details",)
+    implementation = ResourceColumn("implementation",
+                                    verbose_name=_("Implementation"),)
+    component = ResourceColumn("component",
+                               verbose_name=_("Component"),)
+    resource = ResourceColumn("resource",
+                              verbose_name=_("Resource"),)
+
+    def get_object_id(self, resource):
+        return resource.resource_type
+
+    class Meta:
+        name = "resource_types"
+        verbose_name = _("Resource Types")
---
+++ openstack_dashboard/dashboards/project/stacks/resource_types/tests.py
@@ -0,0 +1,52 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.core.urlresolvers import reverse
+from django import http
+
+from mox import IsA  # noqa
+
+from openstack_dashboard import api
+from openstack_dashboard.test import helpers as test
+
+
+class ResourceTypesTests(test.TestCase):
+    INDEX_URL = reverse('horizon:project:stacks.resource_types:index')
+
+    @test.create_stubs({api.heat: ('resource_types_list',)})
+    def test_index(self):
+        api.heat.resource_types_list(
+            IsA(http.HttpRequest)).AndReturn(self.resource_types.list())
+        self.mox.ReplayAll()
+
+        res = self.client.get(self.INDEX_URL)
+        self.assertTemplateUsed(
+            res, 'project/stacks.resource_types/index.html')
+        self.assertContains(res, 'AWS::CloudFormation::Stack')
+
+    @test.create_stubs({api.heat: ('resource_type_get',)})
+    def test_detail_view(self):
+        rt = self.api_resource_types.first()
+
+        api.heat.resource_type_get(
+            IsA(http.HttpRequest), rt['resource_type']).AndReturn(rt)
+        self.mox.ReplayAll()
+
+        url = reverse('horizon:project:stacks.resource_types:details',
+                      args=[rt['resource_type']])
+        res = self.client.get(url)
+
+        self.assertTemplateUsed(
+            res, 'project/stacks.resource_types/details.html')
+        self.assertContains(res, "<h1>Resource Type Details</h1>", 1, 200)
+        self.assertNoMessages()
