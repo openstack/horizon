07df57b3ad64ea8a5bad18933b6a3915d335a6f9
Enrique Garcia Navalon <garcianavalon@gmail.com>
2014-11-17 17:48:26 +0100

    bootstrapping

--- openstack_dashboard/static/horizon/js/horizon.permissions.js
+++ openstack_dashboard/static/horizon/js/horizon.permissions.js
@@ -3,38 +3,36 @@
  */
 
 horizon.permissions = {
-    _loaded_permissions = [], //dictionary to store already loaded permissions
-    active_role = null
+    _all_permissions : [],
+    _role_permissions : [], //dictionary to store already loaded permissions for each role
+    active_role : null
 };
+
+horizon.permissions.render_list = function () {
+    //populate the table with all the permissions
+    //checking the ones that are already assigned to the role
+}
 
 horizon.addInitFunction(function() {
 
- /*// AJAX loading of the role permissions
-  $(document).on('submit', '#create_role_form', function (evt) {
-    
-    //logic
+ // AJAX loading of the role permissions
+  $('input[type=radio][name=activeRole]').change( function (evt) {
+    //get the active one
+        //store it in active_role
+    //check if the permissions lists is loaded
+        //false->ajax load it
+    //check if active role's permission list is already loaded
+        //true-> render the list
+    //false -> ajax request for the list of role-permission
+    //then render the list
+  });
 
-    ajaxOpts = {
-      type: "POST",
-      url: $form.attr('action'),
-      headers: headers,
-      data: formData,
-      beforeSend: function () {
-        console.log('before send');
-      },
-      complete: function () {
-       
-      },
-      success: function (data, textStatus, jqXHR) {
-        console.log('success');
-      },
-      error: function (jqXHR, status, errorThrown) {
-        console.log('error');
-    };
+  // AJAX give/remove permission to role using the checkbox
+  $('.permission_check').change(function (evt) {
+    //get the active permission
+    //if selected
+  });
 
-    $.ajax(ajaxOpts);
-    console.log('ajax!');
-  });*/
-
+  console.log('loaded permissions')
 });
 
