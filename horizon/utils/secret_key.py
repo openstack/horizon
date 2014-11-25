5334e38837790b5d4fe83ebf030567e29e4fb981
George Peristerakis <george.peristerakis@enovance.com>
2014-11-25 16:03:27 -0500

    Fix the expression to be python3 compatible.
    the oct function in python2 and python3 return different results.
    Instead of comparing strings, compare the integer results directly.
    
    Change-Id: Ic802d8dcb27067fa4b1e57eb549565516715c94e

--- horizon/utils/secret_key.py
+++ horizon/utils/secret_key.py
@@ -57,7 +57,7 @@ def generate_or_read_from_file(key_file='.secret_key', key_length=64):
                 f.write(key)
             os.umask(old_umask)
         else:
-            if oct(os.stat(key_file).st_mode & 0o777) != '0600':
+            if (os.stat(key_file).st_mode & 0o777) != 0o600:
                 raise FilePermissionError("Insecure key file permissions!")
             with open(key_file, 'r') as f:
                 key = f.readline()
