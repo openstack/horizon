2adfd0d2a19e17db65d936469f681d162dbd0bb5
Thai Tran <tqtran@us.ibm.com>
2014-12-11 18:03:13 -0800

    Clean modal forms in Identity Roles
    Refactoring identity roles modals.
    Based on merged patch: https://review.openstack.org/#/c/123472/
    
    Change-Id: I435b10456e1a400888cc7aaa3af879543bbff950
    Partially-Implements: blueprint form-template-to-view
    Partially-Closes-Bug: #1350953

--- openstack_dashboard/dashboards/identity/roles/views.py
+++ openstack_dashboard/dashboards/identity/roles/views.py
@@ -51,8 +51,12 @@ class IndexView(tables.DataTableView):
 
 
 class UpdateView(forms.ModalFormView):
-    form_class = project_forms.UpdateRoleForm
     template_name = 'identity/roles/update.html'
+    modal_header = _("Update Role")
+    form_id = "update_role_form"
+    form_class = project_forms.UpdateRoleForm
+    submit_label = _("Update Role")
+    submit_url = "horizon:identity:roles:update"
     success_url = reverse_lazy('horizon:identity:roles:index')
 
     @memoized.memoized_method
@@ -67,7 +71,8 @@ class UpdateView(forms.ModalFormView):
 
     def get_context_data(self, **kwargs):
         context = super(UpdateView, self).get_context_data(**kwargs)
-        context['role'] = self.get_object()
+        args = (self.get_object().id,)
+        context['submit_url'] = reverse(self.submit_url, args=args)
         return context
 
     def get_initial(self):
@@ -77,6 +82,10 @@ class UpdateView(forms.ModalFormView):
 
 
 class CreateView(forms.ModalFormView):
-    form_class = project_forms.CreateRoleForm
     template_name = 'identity/roles/create.html'
+    modal_header = _("Create Role")
+    form_id = "create_role_form"
+    form_class = project_forms.CreateRoleForm
+    submit_label = _("Create Role")
+    submit_url = reverse_lazy("horizon:identity:roles:create")
     success_url = reverse_lazy('horizon:identity:roles:index')
