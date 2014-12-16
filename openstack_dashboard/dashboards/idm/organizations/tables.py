1f0d352673994407d02b90f7d9027a8f4da1949e
Enrique Garcia Navalon <garcianavalon@gmail.com>
2014-12-16 16:02:34 +0100

    add member action

--- openstack_dashboard/dashboards/idm/organizations/tables.py
+++ openstack_dashboard/dashboards/idm/organizations/tables.py
@@ -14,7 +14,6 @@
 
 from django.utils.translation import ugettext_lazy as _
 
-from horizon import forms
 from horizon import tables
 
 
@@ -30,6 +29,7 @@ class OrganizationsTable(tables.DataTable):
         name = "organizations"
         verbose_name = _("Organizations")
 
+
 class MyOrganizationsTable(tables.DataTable):
     name = tables.Column('name', verbose_name=_('Name'))
     description = tables.Column(lambda obj: getattr(obj, 'description', None),
@@ -43,15 +43,25 @@ class MyOrganizationsTable(tables.DataTable):
         verbose_name = _("My Organizations")
 
 
-       
+class AddMemberLink(tables.LinkAction):
+    name = "add"
+    verbose_name = _("Add")
+    url = "horizon:idm:organizations:index"
+    classes = ("ajax-modal",)
+
+    def allowed(self, request, user):
+        # TODO(garcianavalon)
+        return True
+
 class MembersTable(tables.DataTable):
     name = tables.Column('name', verbose_name=_('Members'))
-    clickable = True
     show_avatar = True
 
     class Meta:
         name = "members"
-        verbose_name = _("Members")
+        verbose_name = _("Authorized members")
+        table_actions = (AddMemberLink, )
+        multi_select = False
 
 
 class ApplicationsTable(tables.DataTable):
