69d73ee9c4f97f7b156ff45c1523b6cd9b09f525
Paul Karikh <pkarikh@mirantis.com>
2014-12-22 16:01:09 +0300

    Fix replacement of exception category list
    Make config of ADD_EXCEPTIONS plugin append to the
    exception category list and not replace it.
    
    Change-Id: Ib52012bc93311ac019312b0f92b5fcb8842de53d
    Co-Authored-By: Eric Peterson <eric.peterson1@twcable.com>
    Closes-Bug: #1404032

--- openstack_dashboard/utils/settings.py
+++ openstack_dashboard/utils/settings.py
@@ -98,7 +98,7 @@ def update_dashboards(modules, horizon_config, installed_apps):
 
     enabled_dashboards = []
     disabled_dashboards = []
-    exceptions = {}
+    exceptions = horizon_config.get('exceptions', {})
     apps = []
     angular_modules = []
     js_files = set()
@@ -110,7 +110,10 @@ def update_dashboards(modules, horizon_config, installed_apps):
                 disabled_dashboards.append(config.get('DASHBOARD'))
             continue
         apps.extend(config.get('ADD_INSTALLED_APPS', []))
-        exceptions.update(config.get('ADD_EXCEPTIONS', {}))
+        for category, exc_list in config.get('ADD_EXCEPTIONS', {}).iteritems():
+            exceptions[category] = tuple(set(exceptions.get(category, ())
+                                             + exc_list))
+
         angular_modules.extend(config.get('ADD_ANGULAR_MODULES', []))
         js_files.update(config.get('ADD_JS_FILES', []))
         update_horizon_config.update(
