0932c81584989691e7fbf8ce1f03fe9ddacadc9b
David Lyle <david.lyle@intel.com>
2014-12-29 10:51:21 -0700

    Potential 500 pages on allowed methods removed
    The VPN, Firewall and Loadbalancers panels all have checks to see if the
    appropriate service is enabled. This call requires a neutron API call
    that is not protected in a try/except block. If there is a problem with
    the neutron service, on login, the user encounters the 500 page and is
    unable to do anything in Horizon.
    
    The same is true of checking enabled extensions in the Aggregates panel.
    
    The desired experience is the user should still be able to log in and
    perform actions unrelated to those misconfigured/down services.
    
    Closes-Bug: #1406329
    Change-Id: I8b0eae53c1b8c9a8bc9376e6d813dd6d2bea0e9b

--- openstack_dashboard/dashboards/project/loadbalancers/panel.py
+++ openstack_dashboard/dashboards/project/loadbalancers/panel.py
@@ -10,12 +10,16 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import logging
+
 from django.utils.translation import ugettext_lazy as _
 
 import horizon
 
 from openstack_dashboard.api import neutron
 from openstack_dashboard.dashboards.project import dashboard
+
+LOG = logging.getLogger(__name__)
 
 
 class LoadBalancer(horizon.Panel):
@@ -27,9 +31,15 @@ class LoadBalancer(horizon.Panel):
         request = context['request']
         if not request.user.has_perms(self.permissions):
             return False
-        if not neutron.is_service_enabled(request,
-                                          config_name='enable_lb',
-                                          ext_name='lbaas'):
+        try:
+            if not neutron.is_service_enabled(request,
+                                              config_name='enable_lb',
+                                              ext_name='lbaas'):
+                return False
+        except Exception:
+            LOG.error("Call to list enabled services failed. This is likely "
+                      "due to a problem communicating with the Neutron "
+                      "endpoint. Load Balancers panel will not be displayed.")
             return False
         if not super(LoadBalancer, self).allowed(context):
             return False
