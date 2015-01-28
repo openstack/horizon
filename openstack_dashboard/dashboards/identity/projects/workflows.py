openstack/horizon

commit 257cf49047e1f4826b443650e0f410ffac83190a

Prevent user from disabling her current project
Disabling user's current project is like shooting yourself in the foot
- once it's done, it's irreversible in the current tenant because it's
no longer possible to edit any Project/User/etc. Thus it is forbidden
both when using Edit Project modal or when inline-editing the
corresponding cell at Projects table.

Closes-Bug: #1398331
Change-Id: Iacca383f1d982a9b16c41c9f945fdcdde9f803a1

openstack_dashboard/dashboards/identity/projects/workflows.py
@@ -506,6 +506,14 @@ class CreateProject(workflows.Workflow):
 class UpdateProjectInfoAction(CreateProjectInfoAction):
     enabled = forms.BooleanField(required=False, label=_("Enabled"))
 
+    def __init__(self, request, initial, *args, **kwargs):
+        super(UpdateProjectInfoAction, self).__init__(
+            request, initial, *args, **kwargs)
+        if initial['project_id'] == request.user.token.project['id']:
+            self.fields['enabled'].widget.attrs['disabled'] = True
+            self.fields['enabled'].help_text = _(
+                'You cannot disable your current project')
+
     class Meta:
         name = _("Project Information")
         slug = 'update_info'

openstack_dashboard/dashboards/identity/projects/tables.py
@@ -181,9 +181,12 @@ class UpdateRow(tables.Row):
 
 class UpdateCell(tables.UpdateAction):
     def allowed(self, request, project, cell):
-        return api.keystone.keystone_can_edit_project() and \
-            policy.check((("identity", "identity:update_project"),),
-                         request)
+        policy_rule = (("identity", "identity:update_project"),)
+        return (
+            (cell.column.name != 'enabled' or
+             request.user.token.project['id'] != cell.datum.id) and
+            api.keystone.keystone_can_edit_project() and
+            policy.check(policy_rule, request))
 
     def update_cell(self, request, datum, project_id,
                     cell_name, new_cell_value):

commit 525cf78293fd376b9fe161c04525575ae57e1d77

Fix editing current project
The fix 257cf490 introduced a regression by disabling 'Enable'
checkbox in case of editing current project - because Django treats
disabled checkbox as providing the ``False`` value regardless of its
actual value. Fix it by restoring 'Enable' checkbox value in form's
``clean()`` method.

Change-Id: Iaede347b5a8a06a793de2dd62ba8a695f3498ead
Closes-Bug: #1413068

openstack_dashboard/dashboards/identity/projects/workflows.py
@@ -529,6 +529,18 @@ class UpdateProjectInfoAction(CreateProjectInfoAction):
             self.fields['enabled'].help_text = _(
                 'You cannot disable your current project')
 
+    def clean(self):
+        cleaned_data = super(UpdateProjectInfoAction, self).clean()
+        # NOTE(tsufiev): in case the current project is being edited, its
+        # 'enabled' field is disabled to prevent changing the field value
+        # which is always `True` for the current project (because the user
+        # logged in it). Since Django treats disabled checkbox as providing
+        # `False` value even if its initial value is `True`, we need to
+        # restore the original `True` value of 'enabled' field here.
+        if self.fields['enabled'].widget.attrs.get('disabled', False):
+            cleaned_data['enabled'] = True
+        return cleaned_data
+
     class Meta:
         name = _("Project Information")
         slug = 'update_info'