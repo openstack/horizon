c462fd0af0c6f7041f8d21079754d16aa7c5786d
Enrique Garcia Navalon <garcianavalon@gmail.com>
2015-02-04 17:30:41 +0100

    made idm dashboard default instead of idm_admin

--- openstack_dashboard/local/enabled/_51_idm_admin.py
+++ openstack_dashboard/local/enabled/_51_idm_admin.py
@@ -3,7 +3,7 @@ DASHBOARD = 'idm_admin'
 
 # If set to True, this dashboard will not be added to the settings.
 DISABLED = False
-DEFAULT = True
+DEFAULT = False
 # A list of applications to be added to INSTALLED_APPS.
 ADD_INSTALLED_APPS = [
     'openstack_dashboard.dashboards.idm_admin',
