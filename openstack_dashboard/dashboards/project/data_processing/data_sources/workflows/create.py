14c2ed87f1b9500829ee44d5e8dfe183a2b4ca2d
Chad Roberts <croberts@redhat.com>
2015-02-16 15:50:40 -0500

    [Data processing] Fixing data source create form to be switchable
    The data source creation form is now using the
    switchable/switched mechanism for showing the
    form fields that depend on the data source type.
    
    Change-Id: I7584a0b777cb296bec2d5401e42488df8bd2aa9c
    Closes-Bug: #1422483

--- openstack_dashboard/dashboards/project/data_processing/data_sources/workflows/create.py
+++ openstack_dashboard/dashboards/project/data_processing/data_sources/workflows/create.py
@@ -30,15 +30,29 @@ class GeneralConfigAction(workflows.Action):
     data_source_type = forms.ChoiceField(
         label=_("Data Source Type"),
         choices=[("swift", "Swift"), ("hdfs", "HDFS")],
-        widget=forms.Select(attrs={"class": "data_source_type_choice"}))
+        widget=forms.Select(attrs={
+            "class": "switchable",
+            "data-slug": "ds_type"
+        }))
 
     data_source_url = forms.CharField(label=_("URL"))
 
-    data_source_credential_user = forms.CharField(label=_("Source username"),
-                                                  required=False)
+    data_source_credential_user = forms.CharField(
+        label=_("Source username"),
+        required=False,
+        widget=forms.TextInput(attrs={
+            "class": "switched",
+            "data-switch-on": "ds_type",
+            "data-ds_type-swift": _("Source username")
+        }))
 
     data_source_credential_pass = forms.CharField(
-        widget=forms.PasswordInput(attrs={'autocomplete': 'off'}),
+        widget=forms.PasswordInput(attrs={
+            'class': 'switched',
+            'data-switch-on': 'ds_type',
+            'data-ds_type-swift': _("Source password"),
+            'autocomplete': 'off'
+        }),
         label=_("Source password"),
         required=False)
 
