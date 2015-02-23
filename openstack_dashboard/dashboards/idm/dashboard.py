d5f7ce0ce3f785d2ddd47513ca20fb938ab9be3a
garcianavalon <garcianavalon@gmail.com>
2015-02-23 10:36:59 +0100

    fix responsive sidebar

--- openstack_dashboard/dashboards/idm/dashboard.py
+++ openstack_dashboard/dashboards/idm/dashboard.py
@@ -5,6 +5,7 @@ import horizon
 
 class Idm(horizon.Dashboard):
     name = _("Identity Manager")
+    name_sm = _("IdM")
     slug = "idm"
     panels = ('home', 'organizations', 'myApplications', 'users')  # Add your panels here.
     default_panel = 'home'  # Specify the slug of the dashboard's default panel.
