c682043bea21903584733d1bdb2c816ce2bef80b
ravikumar-venkatesan <ravikumar.venkatesan@hp.com>
2015-02-24 12:09:51 +0000

    Adds floatingip allocate,release integration tests
    This submission adds test_floating.py and floatingipspage.py which
    tests allocate  and release floatingip functionalities based on
    the new regions change.
    
    Partially implements blueprint: selenium-integration-testing
    
    Change-Id: I1992457e015609baa8e7720d94d1539af05b95d4

---
+++ openstack_dashboard/test/integration_tests/tests/test_floatingip.py
@@ -0,0 +1,29 @@
+# Copyright 2015 Hewlett-Packard Development Company, L.P
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from openstack_dashboard.test.integration_tests import helpers
+
+
+class TestFloatingip(helpers.TestCase):
+    """Checks that the user is able to allocate/release floatingip."""
+
+    def test_floatingip(self):
+        floatingip_page = \
+            self.home_pg.go_to_accessandsecurity_floatingipspage()
+        floating_ip = floatingip_page.allocate_floatingip()
+        self.assertTrue(floatingip_page.is_floatingip_present(floating_ip))
+
+        floatingip_page.release_floatingip(floating_ip)
+        self.assertFalse(floatingip_page.is_floatingip_present(floating_ip))
