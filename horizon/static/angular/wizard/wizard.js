39ba4d19da95b5148ff3b0e963fee9ccf746d3a9
Shaoquan Chen <sean.chen2@hp.com>
2015-03-18 08:49:24 -0700

    [Launch Instance Fix] Enhancing wizard widget
    1) Add some events to Wizard widget to provide hooks for logic in model(s)
    and controller(s) for each step.
    
    New wizard events added are:
    - ON_INIT_SUCCESS
    - ON_INIT_ERROR
    - ON_SWITCH
    - BEFORE_SUBMIT
    - AFTER_SUBMIT
    
    Some features in Launch Instance work flow can take advantage of this
    infrastructure change:
    
    - Refreshing page on `AFTER_SUBMIT` event.
    - Lazy data sync and validation on `BEFORE_SUBMIT` event.
    - Dynamical showing/hiding optional steps/content according to async check for
      availability of certain services on `ON_INIT_SUCCESS` event.
    
    2) Add a new feature to check readiness for each step that provides a optional `checkReadiness`
    method. Each such a method should return a promise. When this method is
    provided with the step, the `ready` property of the step will be set to
    `false` until the promise gets resolved. If no `checkReadiness` method
    provided, the `ready` property of the step will be set to `true` directly.
    
    This it useful for those work flow where some step may be optional and
    should be showing to UI conditionally and the check for the condition
    is an asynchronous operation.
    
    Co-Authored-By: Brian Tully <brian.tully@hp.com>
    Closes-Bug: 1433674
    Change-Id: I556e263f4c701a378452b4397585f1b4d8abb763

--- horizon/static/angular/wizard/wizard.js
+++ horizon/static/angular/wizard/wizard.js
@@ -1,6 +1,10 @@
 (function () {
   'use strict';
 
+  var extend = angular.extend,
+      forEach = angular.forEach,
+      noop = angular.noop;
+
   angular.module('hz.widget.wizard', ['ui.bootstrap'])
 
     .constant('wizardLabels', {
@@ -10,33 +14,156 @@
       finish: gettext('Finish')
     })
 
+    .constant('wizardEvents', {
+      ON_INIT_SUCCESS: 'ON_INIT_SUCCESS',
+      ON_INIT_ERROR: 'ON_INIT_ERROR',
+      ON_SWITCH: 'ON_SWITCH',
+      BEFORE_SUBMIT: 'BEFORE_SUBMIT',
+      AFTER_SUBMIT: 'AFTER_SUBMIT'
+    })
+
     .directive('wizard', ['basePath', function (path) {
       return {
-        controller: ['$scope', 'wizardLabels', function ($scope, wizardLabels) {
-          $scope.currentIndex = 0;
-          $scope.openHelp = false;
-          $scope.workflow = $scope.workflow || {};
-          $scope.btnText = angular.extend({}, wizardLabels, $scope.workflow.btnText);
-          $scope.btnIcon = $scope.workflow.btnIcon || {};
-          $scope.steps = $scope.workflow.steps || [];
-          $scope.wizardForm = {};
-          $scope.showSpinner = false;
-          $scope.hasError = false;
-          $scope.switchTo = function (index) {
-            $scope.currentIndex = index;
+        controller: ['$scope', '$q', 'wizardLabels', 'wizardEvents',
+          function ($scope, $q, wizardLabels, wizardEvents) {
+            $scope.currentIndex = -1;
             $scope.openHelp = false;
-          };
-          $scope.showError = function (errorMessage) {
+            $scope.workflow = $scope.workflow || {};
+            $scope.btnText = extend({}, wizardLabels, $scope.workflow.btnText);
+            $scope.btnIcon = $scope.workflow.btnIcon || {};
+            $scope.steps = $scope.workflow.steps || [];
+            $scope.wizardForm = {};
+            $scope.initTask = $q.defer();
+            $scope.initPromise = $scope.initTask.promise;
             $scope.showSpinner = false;
-            $scope.errorMessage = errorMessage;
-            $scope.hasError = true;
-          };
-        }],
-        templateUrl: path + 'wizard/wizard.html'
-      };
-    }])
+            $scope.hasError = false;
 
-    .controller('ModalContainerCtrl', ['$scope', '$modalInstance', 'launchContext', function ($scope, $modalInstance, launchContext) {
+            $scope.switchTo = function (index) {
+              /**
+               * In each step's controller, $scope.$index can be used by the step
+               * to identify itself. For example:
+               *
+               * var comingToMe = (event.to === $scope.$index);
+               */
+              $scope.$broadcast(wizardEvents.ON_SWITCH, {
+                from: $scope.currentIndex,
+                to: index
+              });
+              $scope.currentIndex = index;
+              $scope.openHelp = false;
+            };
+            $scope.showError = function (errorMessage) {
+              $scope.showSpinner = false;
+              $scope.errorMessage = errorMessage;
+              $scope.hasError = true;
+            };
+            $scope.beforeSubmit = function () {
+              $scope.$broadcast(wizardEvents.BEFORE_SUBMIT);
+            };
+            $scope.afterSubmit = function () {
+              $scope.$broadcast(wizardEvents.AFTER_SUBMIT);
+              $scope.close();
+            };
+            $scope.onClickFinishBtn = function () {
+              $scope.beforeSubmit();
+              $scope.submit().then($scope.afterSubmit, $scope.showError);
+            };
+            $scope.initPromise.then(
+              function () {
+                $scope.$broadcast(wizardEvents.ON_INIT_SUCCESS);
+              },
+              function () {
+                $scope.$broadcast(wizardEvents.ON_INIT_ERROR);
+              }
+            );
+
+            /**
+             * Each step in the workflow can provide an optional `checkReadiness`
+             * method, which should return a promise. When this method is provided
+             * with a step, the `.ready` property of the step will be set to
+             * `false` until the promise gets resolved. If no `checkReadiness` method
+             * is provided, the `.ready` property of the step will be set to `true`
+             * by default.
+             *
+             * This is useful for workflows where some steps are optional and/or
+             * displayed to the UI conditionally, and the check for the condition
+             * is an asynchronous operation.
+             *
+             * @return {Promise} This promise gets resolved when all the checking
+             * for each step's promises are done.
+             *
+             * @example
+
+              ```js
+                var launchInstanceWorkFlow = {
+                  //...
+                  steps: [
+                    // ...
+                    {
+                      title: gettext('Network'),
+                      templateUrl: path + 'launch-instance/network/network.html',
+                      helpUrl: path + 'launch-instance/network/network.help.html',
+                      formName: 'launchInstanceNetworkForm',
+
+                      checkReadiness: function () {
+                        var d = $q.defer();
+                        setTimeout(function () {
+                          d.resolve();
+                        }, 500);
+                        return d.promise;
+                      }
+                    }
+                    //...
+                  ],
+                  //...
+                };
+              ```
+             */
+            function checkAllReadiness() {
+              var stepReadyPromises = [];
+
+              forEach($scope.steps, function (step) {
+                step.ready = !step.checkReadiness;
+
+                if (step.checkReadiness) {
+                  var promise = step.checkReadiness();
+                  stepReadyPromises.push(promise);
+                  promise.then(
+                    function () { step.ready = true; },
+                    noop
+                  );
+                }
+              });
+
+              $scope.ready = (stepReadyPromises.length === 0);
+              return $q.all(stepReadyPromises);
+            }
+
+            function switchToFirstReadyStep() {
+              forEach($scope.steps, function (step, index) {
+                if ($scope.currentIndex < 0 && step.ready) {
+                  $scope.currentIndex = index;
+                  return;
+                }
+              });
+            }
+
+            // angular promise doesn't have #always method right now,
+            // this is a simple workaround.
+            function always() {
+              $scope.ready = true;
+              switchToFirstReadyStep();
+            }
+
+            checkAllReadiness().then(always, always);
+          }],
+          templateUrl: path + 'wizard/wizard.html'
+        };
+      }
+    ])
+
+    .controller('ModalContainerCtrl', ['$scope', '$modalInstance', 'launchContext',
+      function ($scope, $modalInstance, launchContext) {
         $scope.launchContext = launchContext;
         $scope.close = function () {
           $modalInstance.close();
