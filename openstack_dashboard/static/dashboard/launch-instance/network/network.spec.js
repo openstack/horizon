23134c637a87b11112e73d8e0f3555aab5caa123
Matt Borland <matt.borland@hp.com>
2015-03-24 08:34:14 -0600

    [Launch Instance Fix] Add Network Step Unit Tests
    This patch adds unit tests for the network steps.  These tests are focused
    around the controller and its related components (no selenium tests or view
    tests).
    
    Change-Id: I0f4b0ef900e5f17c7b39e9f3ca60780e0d26b8e8
    Partial-Bug: 1435869

--- openstack_dashboard/static/dashboard/launch-instance/network/network.spec.js
+++ openstack_dashboard/static/dashboard/launch-instance/network/network.spec.js
@@ -1,4 +1,3 @@
-/* jshint globalstrict: true */
 /*
  *    (c) Copyright 2015 Hewlett-Packard Development Company, L.P.
  *
@@ -18,5 +17,84 @@
   'use strict';
 
   describe('Launch Instance Network Step', function() {
+
+    describe('LaunchInstanceNetworkCtrl', function() {
+      var scope, ctrl;
+
+      beforeEach(module('hz.dashboard.launch-instance'));
+
+      beforeEach(inject(function($controller) {
+        scope = {model: {
+                  newInstanceSpec: {networks: ['net-a']},
+                  networks: ['net-a', 'net-b']}};
+        ctrl = $controller('LaunchInstanceNetworkCtrl', {$scope:scope});
+      }));
+
+      it('has correct network statuses', function() {
+        expect(scope.networkStatuses).toBeDefined();
+        expect(scope.networkStatuses.ACTIVE).toBeDefined();
+        expect(scope.networkStatuses.DOWN).toBeDefined();
+        expect(Object.keys(scope.networkStatuses).length).toBe(2);
+      });
+
+      it('has correct network admin states', function() {
+        expect(scope.networkAdminStates).toBeDefined();
+        expect(scope.networkAdminStates.UP).toBeDefined();
+        expect(scope.networkAdminStates.DOWN).toBeDefined();
+        expect(Object.keys(scope.networkStatuses).length).toBe(2);
+      });
+
+      it('defines a multiple-allocation table', function() {
+        expect(scope.tableLimits).toBeDefined();
+        expect(scope.tableLimits.maxAllocation).toBe(-1);
+      });
+
+      it('contains its own labels', function() {
+        expect(scope.label).toBeDefined();
+        expect(Object.keys(scope.label).length).toBeGreaterThan(0);
+      });
+
+      it('contains help text for the table', function() {
+        expect(scope.tableHelpText).toBeDefined();
+        expect(scope.tableHelpText.allocHelpText).toBeDefined();
+        expect(scope.tableHelpText.availHelpText).toBeDefined();
+      });
+
+      it('uses scope to set table data', function() {
+        expect(scope.tableDataMulti).toBeDefined();
+        expect(scope.tableDataMulti.available).toEqual(['net-a', 'net-b']);
+        expect(scope.tableDataMulti.allocated).toEqual(['net-a']);
+        expect(scope.tableDataMulti.displayedAllocated).toEqual([]);
+        expect(scope.tableDataMulti.displayedAvailable).toEqual([]);
+      });
+    });
+
+    describe('LaunchInstanceNetworkHelpCtrl', function() {
+      var ctrl;
+
+      beforeEach(module('hz.dashboard.launch-instance'));
+
+      beforeEach(inject(function($controller) {
+        ctrl = $controller('LaunchInstanceNetworkHelpCtrl', {});
+      }));
+
+      it('defines the title', function() {
+        expect(ctrl.title).toBeDefined();
+      });
+
+      it('defines paragraphs', function() {
+        expect(ctrl.paragraphs).toBeDefined();
+        expect(ctrl.paragraphs.length).toBeGreaterThan(0);
+      });
+
+      it('defines the network characteristics title', function() {
+        expect(ctrl.networkCharTitle).toBeDefined();
+      });
+
+      it('defines network characteristics paragraphs', function() {
+        expect(ctrl.networkCharParagraphs).toBeDefined();
+        expect(ctrl.networkCharParagraphs.length).toBeGreaterThan(0);
+      });
+    });
   });
 })();
