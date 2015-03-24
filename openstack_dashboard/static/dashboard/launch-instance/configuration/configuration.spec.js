4aa435a69b4fe8cb04e4ff75e39989e57beef95b
Matt Borland <matt.borland@hp.com>
2015-03-24 11:00:17 -0600

    [Launch Instance Fix] Add Configuration Step Unit Tests
    This patch adds unit tests for the configuration step.  These tests are focused
    around the controller and its related components (no selenium tests or view
    tests).
    
    Co-Authored-By: Brian Tully <brian.tully@hp.com>
    Co-Authored-By: Shaoquan Chen <sean.chen2@hp.com>
    Change-Id: Ic25a36ca598459cce4a577da0a99175fbcd1d27a
    Partial-Bug: 1435869

--- openstack_dashboard/static/dashboard/launch-instance/configuration/configuration.spec.js
+++ openstack_dashboard/static/dashboard/launch-instance/configuration/configuration.spec.js
@@ -1,4 +1,3 @@
-/* jshint globalstrict: true */
 /*
  *    (c) Copyright 2015 Hewlett-Packard Development Company, L.P.
  *
@@ -17,7 +16,71 @@
 (function() {
   'use strict';
 
-  describe('Launch Instance Post-Creation Step', function() {
+  describe('Launch Instance Configuration Step', function() {
+
+    describe('LaunchInstanceConfigurationCtrl', function() {
+      var scope, ctrl;
+
+      beforeEach(module('hz.dashboard.launch-instance'));
+
+      beforeEach(inject(function($controller) {
+        scope = {
+          model: {
+            newInstanceSpec: {},
+          }
+        };
+
+        ctrl = $controller('LaunchInstanceConfigurationCtrl', {
+          $scope: scope
+        });
+      }));
+
+      it('contains its own labels', function() {
+        expect(ctrl.label).toBeDefined();
+        expect(Object.keys(ctrl.label).length).toBeGreaterThan(0);
+      });
+
+      it('has correct disk configuration options', function() {
+        expect(ctrl.diskConfigOptions).toBeDefined();
+        expect(ctrl.diskConfigOptions.length).toBe(2);
+        var vals = ctrl.diskConfigOptions.map(function(x) { return x.value; });
+        expect(vals).toContain('AUTO');
+        expect(vals).toContain('MANUAL');
+      });
+
+      it('defaults the disk configuration to "AUTO"', function() {
+        expect(scope.model.newInstanceSpec.disk_config).toBe('AUTO');
+      });
+
+      it('defaults the config_drive configuration to false', function() {
+        expect(scope.model.newInstanceSpec.config_drive).toBe(false);
+      });
+
+      it('defaults the user_data configuration to ""', function() {
+        expect(scope.model.newInstanceSpec.user_data).toBe('');
+      });
+
+    });
+
+    describe('LaunchInstanceConfigurationHelpCtrl', function() {
+      var ctrl;
+
+      beforeEach(module('hz.dashboard.launch-instance'));
+
+      beforeEach(inject(function($controller) {
+        ctrl = $controller('LaunchInstanceConfigurationHelpCtrl');
+      }));
+
+      it('defines the title', function() {
+        expect(ctrl.title).toBeDefined();
+      });
+
+      it('has help paragraphs', function() {
+        expect(ctrl.paragraphs).toBeDefined();
+        expect(ctrl.paragraphs.length).toBeGreaterThan(0);
+      });
+    });
+
   });
 
 })();
