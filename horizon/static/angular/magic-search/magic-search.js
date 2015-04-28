dea9305dc71bd3ba9d90b8608855bd5941253d49
Aaron Sahlin <asahlin@us.ibm.com>
2015-04-28 11:28:41 -0500

    Add description & example for isServer property in Magic Search
    Added description and example of setting isServer property to indicate
    facet is server side and should be rendered as such.
    
    Change-Id: Icbea0ce55b5a51dab9986b0e6488fbf8822f7196
    Closes-bug: #1449651

--- horizon/static/angular/magic-search/magic-search.js
+++ horizon/static/angular/magic-search/magic-search.js
@@ -7,18 +7,40 @@
    * @description
    * A directive to modify / extend Magic Search functionality for use in
    * Horizon.
-   * 1. The base magic search widget makes Foundation (a responsive front-end
+   * 1. The base Magic Search widget makes Foundation (a responsive front-end
    * framework) specific calls in showMenu and hideMenu.  In Horizon we use
    * Bootstrap, therefore we need to override those methods.
    *
-   * 2.  Added 'facetsChanged' listener so we can notify base magic search
+   * 2.  Added 'facetsChanged' listener so we can notify base Magic Search
    * widget that new facets are available so they will be picked up.  (Use
    * if your table has dynamic facets)
    *
-   * 3.  To support the distinguishment between server & client side facets
-   * overrode methods removeFacet & initfacets to emit 'checkFacets' event
-   * so implementors can add propterty isServer to facets.  (what keys the
-   * facet icon and color difference)
+   * 3.  Due to the current inconsistencies in the APIs, where some support
+   * filtering and others do not, we wanted a way to distinguish client-side
+   * filtering (searching the visible subset) vs server-side filtering
+   * (another server filter query).
+   *
+   * To support this distinguishment we overrode methods removeFacet &
+   * initfacets to emit 'checkFacets' event so implementers can add property
+   * 'isServer' to facets (what triggers the facet icon and color difference).
+   *
+   * Each table that incorporates Magic Search is responsible for adding
+   * property 'isServer' to their facets as they have the intimate knowledge
+   * of the API supplying the table data.  The setting of this property needs
+   * to be done in the Magic Search supporting JavaScript for each table.
+   *
+   * Example:
+   * // Set property 'isServer' on facets that you want to render as server
+   * // facet (server icon, lighter grey color).  Note: If the property
+   * // 'isServer' is not set, then facet renders with client icon and darker
+   * // grey color.
+   * scope.$on('checkFacets', function(event, currentSearch) {
+   *   angular.forEach(currentSearch, function(facet) {
+   *     if (apiVersion < 3) {
+   *       facet.isServer = true;
+   *     }
+   *   });
+   * });
    *
    * 4.  Overrode initfacets to fix refresh / bookmark issue where facets
    * menu wasn't removing facets that were already on url.
