8ed6a85b85724a3838bd1fd487346e9a966cf5a6
Luigi Toscano <ltoscano@redhat.com>
2015-06-19 18:15:28 +0200

    Wait until the image is registered in Sahara image test
    The tests consistently fails, probably because the availability
    of the image in the table is checked too soon. Add a bit of delay
    with a new wait method.
    
    Change-Id: I9e39c35fb67455b4e4f3c2b12265b8540420ad01
    Closes-Bug: #1466744
    (cherry picked from commit 86d2d4a11be0b5a87e8464e45114c41d934be690)

--- openstack_dashboard/test/integration_tests/tests/test_sahara_image_registry.py
+++ openstack_dashboard/test/integration_tests/tests/test_sahara_image_registry.py
@@ -16,7 +16,6 @@ from openstack_dashboard.test.integration_tests.tests import decorators
 IMAGE_NAME = helpers.gen_random_resource_name("image")
 
 
-@decorators.skip_because(bugs=["1467950"])
 @decorators.services_required("sahara")
 class TestSaharaImageRegistry(helpers.TestCase):
 
@@ -31,6 +30,7 @@ class TestSaharaImageRegistry(helpers.TestCase):
         image_reg_pg = self.home_pg.go_to_dataprocessing_imageregistrypage()
         image_reg_pg.register_image(IMAGE_NAME, self.CONFIG.scenario.ssh_user,
                                     "Test description")
+        image_reg_pg.wait_until_image_registered(IMAGE_NAME)
         self.assertTrue(image_reg_pg.is_image_registered(IMAGE_NAME),
                         "Image was not registered.")
         self.assertFalse(image_reg_pg.is_error_message_present(),
