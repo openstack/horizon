e0fc46e9ab161442a1e71aaa03114e9e678e6f76
Thai Tran <tqtran@us.ibm.com>
2015-06-24 14:53:17 -0700

    Adding Angular Identity Dashboard
    This patch creates a blank Identity dashboard.
    Later on, we will populate this dashboard with Users Panel.
    
    Change-Id: I909a7b8781934e68c29b5058f89a2cc03a88103d
    Partially-Implements: blueprint angularize-identity-tables

--- openstack_dashboard/enabled/_25_identity.py
+++ openstack_dashboard/enabled/_25_identity.py
@@ -1,3 +1,15 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 # The slug of the dashboard to be added to HORIZON['dashboards']. Required.
 DASHBOARD = 'identity'
 # If set to True, this dashboard will be set as the default dashboard.
@@ -6,3 +18,20 @@ DEFAULT = False
 ADD_EXCEPTIONS = {}
 # A list of applications to be added to INSTALLED_APPS.
 ADD_INSTALLED_APPS = ['openstack_dashboard.dashboards.identity']
+
+ADD_ANGULAR_MODULES = [
+    'hz.dashboard',
+    'hz.dashboard.identity',
+]
+
+ADD_JS_FILES = [
+    'dashboard/identity/identity.module.js',
+]
+
+ADD_JS_SPEC_FILES = [
+    'dashboard/identity/identity.module.spec.js',
+]
+
+ADD_SCSS_FILES = [
+    'dashboard/identity/identity.scss'
+]
