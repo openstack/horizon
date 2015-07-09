a74c20a4a40d43ea6df8044e438bbb556486c8e4
Travis Tripp <travis.tripp@hp.com>
2015-07-09 14:17:08 -0600

    Add Angular Basic Page Header Directive
    This provide an angular basic page header directive
    and adds it to the ng images page.
    
    We're addressing future tech debt here by breaking out the predicted
    common pattern of page header HTML into a single angular directive.
    
    This allows for templating with parameterization, so that if in the future
    we or a deployer would like to customize all of their page headers,
    they only need to do it in the angular template HTML.  It also makes
    the usage of it very declarative in nature.
    
    This is basically an angular based way to replace the current django template:
    horizon/templates/horizon/common/_page_header.html
    
    To test set DISABLED = False in _203_project_images_panel.py
    
    Then go to the last images panel in the compute menu.
    
    Change-Id: Ia753905f805af72535bd255887494a6954546b56
    Partially-Implements: blueprint angularize-images-table

---
+++ horizon/static/framework/widgets/headers/hz-page-header-directive.js
@@ -0,0 +1,63 @@
+/*
+ * (c) Copyright 2015 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function () {
+  'use strict';
+
+  angular
+    .module("horizon.framework.widgets.headers")
+    .directive('hzPageHeader', hzPageHeader);
+
+  hzPageHeader.$inject = ['horizon.framework.widgets.headers.basePath'];
+
+  /**
+   * @ngdoc directive
+   * @name hzPageHeader
+   * @description
+   * Provides markup for a general page header.  It takes a title
+   * and description and transcludes any given markup.
+   *
+   * @example
+   *
+   * Default usage to provide a title header and a description that are
+   * translated by the angular gettext translate filter.:
+   *
+   * <hz-page-header header="'My Header' | translate" description="'foo' | translate"/>
+   *
+   * If you have additional content that you want to include beneath the
+   * title header and description, then include that content inside the
+   * directives. Example:
+   *
+   * <hz-page-header header="'My Header' | translate" description="'foo' | translate">
+   *   <a href="http://www.openstack.org">Go to OpenStack</a>
+   * </hz-page-header>
+   */
+  function hzPageHeader(basePath) {
+    var directive = {
+      restrict: 'E',
+      scope: {
+        header: '=',
+        description: '='
+      },
+      templateUrl: basePath + 'hz-page-header.html',
+      transclude: true
+    };
+
+    return directive;
+  }
+
+})();
+
