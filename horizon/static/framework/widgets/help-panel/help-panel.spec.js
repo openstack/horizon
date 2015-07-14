51f5bb2573ba9ebd623b2c6f0ea04a03c2a485e4
Rajat Vig <rajatv@thoughtworks.com>
2015-07-14 10:31:09 -0700

    JSCS Cleanup - use $apply instead of $digest in help-panel spec
    As recommended by eslint rules, changed the spec for
    help-panel to use $apply instead of $digest.
    Also, added Apache License for the JS files.
    
    Change-Id: I42c6fff52e15bea098faf3805ec892ee98491979
    Partially-Implements: blueprint jscs-cleanup

--- horizon/static/framework/widgets/help-panel/help-panel.spec.js
+++ horizon/static/framework/widgets/help-panel/help-panel.spec.js
@@ -1,3 +1,17 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 (function () {
   'use strict';
 
@@ -19,7 +33,7 @@
       $scope = $injector.get('$rootScope').$new();
       $compile = $injector.get('$compile');
       element = $compile('<help-panel>Help</help-panel>')($scope);
-      $scope.$digest();
+      $scope.$apply();
     }));
 
     it('should be compiled', function () {
@@ -33,15 +47,15 @@
 
     it('should add "open" to class name if $scope.openHelp is true', function () {
       $scope.openHelp = true;
-      $scope.$digest();
+      $scope.$apply();
       expect(element[0].querySelector('.help-panel').className).toBe('help-panel open');
     });
 
     it('should remove "open" from class name if $scope.openHelp is false', function () {
       $scope.openHelp = true;
-      $scope.$digest();
+      $scope.$apply();
       $scope.openHelp = false;
-      $scope.$digest();
+      $scope.$apply();
       expect(element[0].querySelector('.help-panel').className).toBe('help-panel');
     });
   });
