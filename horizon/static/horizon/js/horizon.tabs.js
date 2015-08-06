d93f89a9010f79e315d58e079e1a0dee8dbffeff
Justin Pomeroy <jpomero@linux.vnet.ibm.com>
2015-08-06 14:32:19 -0500

    Fix errors on keyboard nav across tabs on forms
    There is code in place to allow using the Tab / Shift+Tab keys to be
    used for navigating across all tabs within a form. This fixes a bad
    reference in this code and restores the intended behavior.
    
    The problem occurs because the code assumes the implicit "event"
    variable exists, but that is not always the case. Specifically this
    seems to be a problem in Firefox. Instead of relying on the implicit
    variable the event object should be referenced using the "evt"
    parameter.
    
    Closes-Bug: #1482353
    Change-Id: I15812693a3d89c0862067119293004e97139040a

--- horizon/static/horizon/js/horizon.tabs.js
+++ horizon/static/horizon/js/horizon.tabs.js
@@ -75,7 +75,7 @@ horizon.addInitFunction(horizon.tabs.init = function () {
     var $this = $(this),
       next_pane = $this.closest(".tab-pane").next(".tab-pane");
     // Capture the forward-tab keypress if we have a next tab to go to.
-    if (evt.which === 9 && !event.shiftKey && next_pane.length) {
+    if (evt.which === 9 && !evt.shiftKey && next_pane.length) {
       evt.preventDefault();
       $(".nav-tabs a[data-target='#" + next_pane.attr("id") + "']").tab('show');
     }
@@ -84,7 +84,7 @@ horizon.addInitFunction(horizon.tabs.init = function () {
     var $this = $(this),
       prev_pane = $this.closest(".tab-pane").prev(".tab-pane");
     // Capture the forward-tab keypress if we have a next tab to go to.
-    if (event.shiftKey && evt.which === 9 && prev_pane.length) {
+    if (evt.shiftKey && evt.which === 9 && prev_pane.length) {
       evt.preventDefault();
       $(".nav-tabs a[data-target='#" + prev_pane.attr("id") + "']").tab('show');
       prev_pane.find(":input:last").focus();
