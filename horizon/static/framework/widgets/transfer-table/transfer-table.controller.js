b68758740a1c8d0419a41fc94b74556fa647b3f6
Thai Tran <tqtran@us.ibm.com>
2015-08-21 12:57:32 -0700

    Cleaning up the Transfer Table
    This patch cleaned the transfer-table code and moved the tooltip logic out.
    This will make it easier to later add the cloning feature in the bp below.
    
    Change-Id: I6d4cb292a4b86abbf453843ba8b96c3b4a6cfa24
    Partially-Implements: blueprint transfer-table-clone-feature

--- horizon/static/framework/widgets/transfer-table/transfer-table.controller.js
+++ horizon/static/framework/widgets/transfer-table/transfer-table.controller.js
@@ -82,148 +82,87 @@
     }
 
     var ctrl = this;
+    ctrl.allocatedIds = {};
+    ctrl.available = {
+      sourceItems: trModel.available,
+      displayedItems: trModel.displayedAvailable
+    };
+    ctrl.allocated = {
+      sourceItems: trModel.allocated,
+      displayedItems: trModel.displayedAllocated
+    };
 
     ctrl.allocate = allocate;
     ctrl.deallocate = deallocate;
     ctrl.toggleView = toggleView;
     ctrl.updateAllocated = updateAllocated;
     ctrl.numAllocated = numAllocated;
-    ctrl.numDisplayedAvailable = numDisplayedAvailable;
 
     ctrl.helpText = angular.extend({}, helpText, trHelpText);
     ctrl.limits = angular.extend({}, limits, trLimits);
-    ctrl.numAvailable = trModel.available ? trModel.available.length : 0;
+    ctrl.numAvailable = numAvailable;
     ctrl.views = { allocated: true, available: true };
 
-    // Tooltip model
-    ctrl.tooltipModel = {
-      templateUrl: path + 'action-list/warning-tooltip.html',
-      data: {
-        clickMessage: gettext('Click here to expand the row and view the errors.'),
-        expandDetail: expandDetail
-      }
-    };
-
-    // Update tracking of allocated IDs when allocated changed
-    $scope.$watchCollection(allocatedCollection, onNewAllocated);
-
-    // Update available count when available changed
-    $scope.$watchCollection(availableCollection, onNewAvailable);
-
-    // Initialize tracking of allocated IDs
-    setAllocatedIds(trModel.allocated);
+    init();
 
     //////////
 
-    // helper function
-    function expandDetail() {
-      /*eslint-disable angular/ng_controller_as_vm */
-      // 'this' referred here is the this for the function not the controller
-      var row = this.element.closest('tr');
-      /*eslint-enable angular/ng_controller_as_vm */
-      if (!row.hasClass('expanded')) {
-        // Timeout needed to prevent
-        // $apply already in progress error
-        $timeout(function() {
-          row.find('[hz-expand-detail]').click();
-        }, 0, false);
-      }
+    function init() {
+
+      // populate the allocatedIds if allocated source given
+      angular.forEach(ctrl.allocated.sourceItems, function(item) {
+        ctrl.allocatedIds[item.id] = true;
+      });
     }
 
-    function setAllocatedIds(allocatedRows) {
-      ctrl.allocatedIds = {};
-      if (allocatedRows) {
-        angular.forEach(allocatedRows, function(alloc) {
-          ctrl.allocatedIds[alloc.id] = true;
-        });
-
-        if (trModel.available) {
-          ctrl.numAvailable = trModel.available.length - allocatedRows.length;
-        } else {
-          ctrl.numAvailable = 0;
-        }
-      } else {
-        trModel.allocated = [];
-        trModel.displayedAllocated = [];
-      }
-    }
-
-    function allocate(row) {
+    function allocate(item) {
+      // Add to allocated only if limit not reached
       if (ctrl.limits.maxAllocation < 0 ||
-          trModel.allocated.length < ctrl.limits.maxAllocation) {
-        // Add to allocated only if limit not reached
-        trModel.allocated.push(row);
-
-        ctrl.numAvailable -= 1;
-      } else if (ctrl.limits.maxAllocation === 1) {
-        // Swap out rows if only one allocation allowed
-        trModel.allocated.pop();
-
+          ctrl.limits.maxAllocation > ctrl.allocated.sourceItems.length) {
+        ctrl.allocated.sourceItems.push(item);
+        ctrl.allocatedIds[item.id] = true;
+      }
+      // Swap out items if only one allocation allowed
+      else if (ctrl.limits.maxAllocation === 1) {
+        var temp = ctrl.allocated.sourceItems.pop();
+        delete ctrl.allocatedIds[temp.id];
         // When swapping out, Smart-Table $watch is
-        // not detecting change so timeout is used
-        // as workaround.
+        // not detecting change so timeout is used as workaround.
         $timeout(function() {
-          trModel.allocated.push(row);
+          ctrl.allocated.sourceItems.push(item);
+          ctrl.allocatedIds[item.id] = true;
           $scope.$apply();
         }, 0, false);
       }
     }
 
-    function deallocate(row) {
-      ctrl.numAvailable += 1;
-
-      var allocLen = trModel.allocated.length;
-      for (var i = allocLen - 1; i >= 0; i--) {
-        if (trModel.allocated[i].id === row.id) {
-          trModel.allocated.splice(i, 1);
-        }
+    // move item from from allocated to available
+    function deallocate(item) {
+      var index = ctrl.allocated.sourceItems.indexOf(item);
+      if (index >= 0) {
+        ctrl.allocated.sourceItems.splice(index, 1);
+        delete ctrl.allocatedIds[item.id];
       }
     }
 
-    // Show/hide allocated or available sections
-    function toggleView(view) {
-      var show = ctrl.views[view];
-      ctrl.views[view] = !show;
+    // update allocated when users re-order via drag-and-drop
+    function updateAllocated(event, item, orderedItems) {
+      ctrl.allocated.sourceItems.splice(0, ctrl.allocated.sourceItems.length);
+      Array.prototype.push.apply(ctrl.allocated.sourceItems, orderedItems);
     }
 
-    // Allocated array needs to be updated when rows re-ordered
-    function updateAllocated(e, item, orderedItems) {
-      var allocLen = trModel.allocated.length;
-      trModel.allocated.splice(0, allocLen);
-      Array.prototype.push.apply(trModel.allocated, orderedItems);
+    /////////////
+
+    function toggleView(view) {
+      ctrl.views[view] = !ctrl.views[view];
     }
 
     function numAllocated() {
-      return trModel.allocated ? trModel.allocated.length : 0;
+      return ctrl.allocated.sourceItems.length;
     }
 
-    function numDisplayedAvailable() {
-      if (trModel.displayedAvailable) {
-        var filtered = trModel.displayedAvailable.filter(function(avail) {
-          return !ctrl.allocatedIds[avail.id];
-        });
-
-        return filtered.length;
-      }
-      return 0;
-    }
-
-    function allocatedCollection() {
-      return trModel.allocated;
-    }
-
-    function onNewAllocated(newAllocated) {
-      setAllocatedIds(newAllocated);
-    }
-
-    function availableCollection() {
-      return trModel.available;
-    }
-
-    function onNewAvailable(newAvailable) {
-      var numAvailable = newAvailable ? newAvailable.length : 0;
-      var numAllocated = trModel.allocated ? trModel.allocated.length : 0;
-      ctrl.numAvailable = numAvailable - numAllocated;
+    function numAvailable() {
+      return ctrl.available.sourceItems.length - ctrl.allocated.sourceItems.length;
     }
   }
 
