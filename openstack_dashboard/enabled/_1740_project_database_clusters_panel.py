57295f42ba30f37357f2dc5713a87f400bfa2134
Sushil Kumar <skm.net@gmail.com>
2015-08-26 09:51:04 +0000

    Initial support for database clustering in Horizon
    Added a separate panel for clusters.
    
    This panel contains the Clusters Table with table actions to
    Launch and Terminate clusters.  There are row actions Add Shard
    and Reset Password (and their associated dialogs) that are specific
    to MongoDB and Vertica respectively.
    
    The Clusters Details will include the following tabs:
    - Overview
    - Instances (table of instances belonging to this cluster)
    
    The launch panel has custom fields for MongoDB and Vertica.
    The custom fields will be dynamically shown based on the datastore
    selected.
    
    Added a db_capability utility to aid in identifying the specific
    datastores.
    
    Added network selection dropdown if neutron is enabled.
    
    Co-Authored-By: Sushil Kumar <sushil.kumar3@hp.com>
    Co-Authored-By: Saurabh Surana <saurabh.surana@hp.com>
    Co-Authored-By: Duk Loi <duk@tesora.com>
    Co-Authored-By: Anna Shen <ruiyuan.shen@hp.com>
    Change-Id: I047f4d37449070adfd0ea66ad010982f35c049aa
    Implements: blueprint database-clustering-support

---
+++ openstack_dashboard/enabled/_1740_project_database_clusters_panel.py
@@ -0,0 +1,25 @@
+# Copyright [2015] Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# The slug of the panel to be added to HORIZON_CONFIG. Required.
+PANEL = 'database_clusters'
+# The slug of the dashboard the PANEL associated with. Required.
+PANEL_DASHBOARD = 'project'
+# The slug of the panel group the PANEL is associated with.
+PANEL_GROUP = 'database'
+
+# Python panel class of the PANEL to be added.
+ADD_PANEL = ('openstack_dashboard.contrib.trove.'
+             'content.database_clusters.panel.Clusters')
