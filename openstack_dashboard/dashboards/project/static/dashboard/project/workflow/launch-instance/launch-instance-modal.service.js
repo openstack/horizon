222bd716fa8e511d8d8d61067625eacb0cca70c8
Rajat Vig <rajatv@thoughtworks.com>
2015-10-09 23:24:52 -0700

    Launch Instance Modal should be exist as a Service
    Extracted openLaunchInstanceWizard from the
    LaunchInstanceModalController to a Service named
    horizon.dashboard.project.workflow.launch-instance.modal.service
    
    This service is required for the new Images panel
    to create the launch instance modal pop up.
    
    Partially-Implements: blueprint angularize-images-table
    Change-Id: Id7177cbafbfb9094d03908b5f8eba52d00241817

---
+++ openstack_dashboard/dashboards/project/static/dashboard/project/workflow/launch-instance/launch-instance-modal.service.js
@@ -0,0 +1,66 @@
+/*
+ *    (c) Copyright 2015 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('horizon.dashboard.project.workflow.launch-instance')
+    .factory(
+      'horizon.dashboard.project.workflow.launch-instance.modal.service',
+      LaunchInstanceModalService
+    );
+
+  LaunchInstanceModalService.$inject = [
+    '$modal',
+    '$window',
+    'horizon.dashboard.project.workflow.launch-instance.modal-spec'
+  ];
+
+  function LaunchInstanceModalService($modal, $window, modalSpec) {
+    var service = {
+      open: open
+    };
+
+    return service;
+
+    function open(launchContext) {
+      var localSpec = {
+        resolve: {
+          launchContext: function () {
+            return launchContext;
+          }
+        }
+      };
+
+      angular.extend(localSpec, modalSpec);
+
+      var launchInstanceModal = $modal.open(localSpec);
+      var handleModalClose = function (redirectPropertyName) {
+        return function () {
+          if (launchContext && launchContext[redirectPropertyName]) {
+            $window.location.href = launchContext[redirectPropertyName];
+          }
+        };
+      };
+
+      launchInstanceModal.result.then(
+        handleModalClose('successUrl'),
+        handleModalClose('dismissUrl')
+      );
+    }
+  }
+
+})();
