c68b2bb090800e8ff2ea7c5048b3aea616e3622f
lin-hua-cheng <os.lcheng@gmail.com>
2015-11-12 11:38:41 -0800

    Fix config ordering for identity plugin file
    The comment for DASHBOARD and GROUP config got switched.
    
    Change-Id: Ia5f2db2f2d91a7109b6b665ef272e5bd6c9962ca

--- openstack_dashboard/enabled/_3010_identity_domains_panel.py
+++ openstack_dashboard/enabled/_3010_identity_domains_panel.py
@@ -1,9 +1,9 @@
 # The slug of the panel to be added to HORIZON_CONFIG. Required.
 PANEL = 'domains'
 # The slug of the dashboard the PANEL associated with. Required.
-PANEL_GROUP = 'default'
-# The slug of the panel group the PANEL is associated with.
 PANEL_DASHBOARD = 'identity'
+# The slug of the panel group the PANEL is associated with.
+PANEL_GROUP = 'default'
 
 # Python panel class of the PANEL to be added.
 ADD_PANEL = 'openstack_dashboard.dashboards.identity.domains.panel.Domains'
