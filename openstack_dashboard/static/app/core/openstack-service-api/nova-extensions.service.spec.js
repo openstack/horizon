4e52904000dc1a01fb4e513a58d2b05e2f3b02eb
Michael Krotscheck <krotscheck@gmail.com>
2015-11-23 10:36:05 -0800

    Enable semi-spacing
    This patch enables the "semi-spacing" rule from eslint-config-openstack,
    and corrects any errors found.
    
    http://eslint.org/docs/rules/semi-spacing
    
    Change-Id: Ib6a2d856f24b53b24c65c4165cec92a7c1428832

--- openstack_dashboard/static/app/core/openstack-service-api/nova-extensions.service.spec.js
+++ openstack_dashboard/static/app/core/openstack-service-api/nova-extensions.service.spec.js
@@ -23,9 +23,9 @@
     beforeEach(module('horizon.app.core.openstack-service-api'));
 
     beforeEach(module(function($provide) {
-      novaAPI = {getExtensions: function() {return {then: angular.noop};}};
+      novaAPI = {getExtensions: function() {return {then: angular.noop}; }};
       q = {defer: function() { return {resolve: angular.noop}; }};
-      $provide.value('$cacheFactory', function() {return "cache";});
+      $provide.value('$cacheFactory', function() {return "cache"; });
       $provide.value('$q', q);
       $provide.value('horizon.app.core.openstack-service-api.nova', novaAPI);
     }));
