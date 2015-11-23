4fbe4af80ee1c7e187218b514cb6734b64f38b38
David Lyle <david.lyle@intel.com>
2015-11-23 10:38:39 -0700

    Excising Sahara from Horizon
    The content is proposed to the openstack/sahara-dashboard repo.
    
    Change-Id: Ie76b09b9bdbdb975c1cd39658f02bcf95b98b961

--- openstack_dashboard/enabled/_1810_data_processing_panel_group.py
+++
@@ -1,8 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-# The slug of the panel group to be added to HORIZON_CONFIG. Required.
-PANEL_GROUP = 'data_processing'
-# The display name of the PANEL_GROUP. Required.
-PANEL_GROUP_NAME = _('Data Processing')
-# The slug of the dashboard the PANEL_GROUP associated with. Required.
-PANEL_GROUP_DASHBOARD = 'project'
