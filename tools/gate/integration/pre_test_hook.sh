1976cb902d8cd9a9b02f68a26dcac81cbd20e17b
Mark Vanderwiel <vanderwl@us.ibm.com>
2016-01-27 16:47:47 -0600

    Add trace to integration gate scripts
    The set -x enabled line-by-line tracing within a bash
    script. This makes it easier to follow gate logs.
    
    Change-Id: I7a164cf94f661929d96f2c3d41ec0d2ad1530e32

--- tools/gate/integration/pre_test_hook.sh
+++ tools/gate/integration/pre_test_hook.sh
@@ -2,6 +2,8 @@
 
 # This script will be executed inside pre_test_hook function in devstack gate
 
+set -x
+
 cd /opt/stack/new/horizon/openstack_dashboard/local/local_settings.d
 mv _20_integration_tests_scaffolds.py.example _20_integration_tests_scaffolds.py
 sudo service apache2 restart
