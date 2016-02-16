062bc25b68aeafe175d5bdb3a2f54233794a5f78
Justin Pomeroy <jpomero@linux.vnet.ibm.com>
2016-02-16 16:08:51 -0600

    Add REST API for working with floating IPs
    This adds the REST APIs for certain floating IP actions such as
    listing floating IP addresses and pools, allocating new floating IPs
    and associating and disassociating floating IPs.
    
    Related to blueprint horizon-lbaas-v2-ui
    Change-Id: I80e550a04b0a0f2ad05203180781013ba5501472

--- openstack_dashboard/api/rest/network.py
+++ openstack_dashboard/api/rest/network.py
@@ -1,5 +1,6 @@
 
 # Copyright 2015, Hewlett-Packard Development Company, L.P.
+# Copyright 2016 IBM Corp.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -44,3 +45,78 @@ class SecurityGroups(generic.View):
         security_groups = api.network.security_group_list(request)
 
         return {'items': [sg.to_dict() for sg in security_groups]}
+
+
+@urls.register
+class FloatingIP(generic.View):
+    """API for a single floating IP address.
+    """
+    url_regex = r'network/floatingip/$'
+
+    @rest_utils.ajax(data_required=True)
+    def post(self, request):
+        """Allocate a new floating IP address.
+
+        :param pool_id: The ID of the floating IP address pool in which to
+                        allocate the new address.
+
+        :return: JSON representation of the new floating IP address
+        """
+        pool = request.DATA['pool_id']
+        result = api.network.tenant_floating_ip_allocate(request, pool)
+        return result.to_dict()
+
+    @rest_utils.ajax(data_required=True)
+    def patch(self, request):
+        """Associate or disassociate a floating IP address.
+
+        :param address_id: The ID of the floating IP address to associate
+                           or disassociate.
+        :param port_id: The ID of the port to associate.
+        """
+        address = request.DATA['address_id']
+        port = request.DATA.get('port_id')
+        if port is None:
+            api.network.floating_ip_disassociate(request, address)
+        else:
+            api.network.floating_ip_associate(request, address, port)
+
+
+@urls.register
+class FloatingIPs(generic.View):
+    """API for floating IP addresses.
+    """
+    url_regex = r'network/floatingips/$'
+
+    @rest_utils.ajax()
+    def get(self, request):
+        """Get a list of floating IP addresses.
+
+        The listing result is an object with property "items". Each item is
+        an extension.
+
+        Example:
+        http://localhost/api/network/floatingips
+        """
+        result = api.network.tenant_floating_ip_list(request)
+        return {'items': [ip.to_dict() for ip in result]}
+
+
+@urls.register
+class FloatingIPPools(generic.View):
+    """API for floating IP pools.
+    """
+    url_regex = r'network/floatingippools/$'
+
+    @rest_utils.ajax()
+    def get(self, request):
+        """Get a list of floating IP pools.
+
+        The listing result is an object with property "items". Each item is
+        an extension.
+
+        Example:
+        http://localhost/api/network/floatingippools
+        """
+        result = api.network.floating_ip_pools_list(request)
+        return {'items': [p.to_dict() for p in result]}
