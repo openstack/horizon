8d32b0a60de21dbb16d9b72738ddea2afa118f2e
jlopezgu <juan.pablo.lopez.gutierrez@intel.com>
2016-04-15 10:56:10 -0500

    Security group add/delete rules integration tests
    Added tests to validate add/delete rules in security groups
    
    Change-Id: If2ec1108cdcc59d9ce7f24a07ead82e071d49ccf
    Implements: blueprint horizon-integration-tests-coverage

--- openstack_dashboard/test/integration_tests/tests/test_security_groups.py
+++ openstack_dashboard/test/integration_tests/tests/test_security_groups.py
@@ -10,12 +10,57 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import random
+
 from openstack_dashboard.test.integration_tests import helpers
 from openstack_dashboard.test.integration_tests.regions import messages
 
 
 class TestSecuritygroup(helpers.TestCase):
-    SECURITYGROUP_NAME = helpers.gen_random_resource_name("securitygroup")
+    SEC_GROUP_NAME = helpers.gen_random_resource_name("securitygroup")
+    RULE_PORT = str(random.randint(9000, 9999))
+
+    @property
+    def securitygroup_page(self):
+        return self.home_pg.\
+            go_to_compute_accessandsecurity_securitygroupspage()
+
+    def _create_securitygroup(self):
+        page = self.securitygroup_page
+        page.create_securitygroup(self.SEC_GROUP_NAME)
+        self.assertTrue(page.find_message_and_dismiss(messages.SUCCESS))
+        self.assertFalse(page.find_message_and_dismiss(messages.ERROR))
+        self.assertTrue(page.is_securitygroup_present(self.SEC_GROUP_NAME))
+
+    def _delete_securitygroup(self):
+        page = self.securitygroup_page
+        page.delete_securitygroup(self.SEC_GROUP_NAME)
+        self.assertTrue(page.find_message_and_dismiss(messages.SUCCESS))
+        self.assertFalse(page.find_message_and_dismiss(messages.ERROR))
+        self.assertFalse(page.is_securitygroup_present(self.SEC_GROUP_NAME))
+
+    def _add_rule(self):
+        page = self.securitygroup_page
+        page = page.go_to_manage_rules(self.SEC_GROUP_NAME)
+        page.create_rule(self.RULE_PORT)
+        self.assertTrue(page.find_message_and_dismiss(messages.SUCCESS))
+        self.assertTrue(page.is_port_present(self.RULE_PORT))
+
+    def _delete_rule_by_table_action(self):
+        page = self.securitygroup_page
+        page = page.go_to_manage_rules(self.SEC_GROUP_NAME)
+        page.delete_rules(self.RULE_PORT)
+        self.assertTrue(page.find_message_and_dismiss(messages.SUCCESS))
+        self.assertFalse(page.find_message_and_dismiss(messages.ERROR))
+        self.assertFalse(page.is_port_present(self.RULE_PORT))
+
+    def _delete_rule_by_row_action(self):
+        page = self.securitygroup_page
+        page = page.go_to_manage_rules(self.SEC_GROUP_NAME)
+        page.delete_rule(self.RULE_PORT)
+        self.assertTrue(page.find_message_and_dismiss(messages.SUCCESS))
+        self.assertFalse(page.find_message_and_dismiss(messages.ERROR))
+        self.assertFalse(page.is_port_present(self.RULE_PORT))
 
     def test_securitygroup_create_delete(self):
         """tests the security group creation and deletion functionalities:
@@ -25,22 +70,39 @@ class TestSecuritygroup(helpers.TestCase):
         * verifies the security group does not appear in the table after
         deletion
         """
+        self._create_securitygroup()
+        self._delete_securitygroup()
 
-        securitygroups_page = \
-            self.home_pg.go_to_compute_accessandsecurity_securitygroupspage()
+    def test_managerules_create_delete_by_row(self):
+        """tests the manage rules creation and deletion funcionalities:
+        * create a new security group
+        * verifies the security group appears in the security groups table
+        * creates a new rule
+        * verifies the rule appears in the rules table
+        * delete the newly created rule
+        * verifies the rule does not appear in the table after deletion
+        * deletes the newly created security group
+        * verifies the security group does not appear in the table after
+        deletion
+        """
+        self._create_securitygroup()
+        self._add_rule()
+        self._delete_rule_by_row_action()
+        self._delete_securitygroup()
 
-        securitygroups_page.create_securitygroup(self.SECURITYGROUP_NAME)
-        self.assertTrue(
-            securitygroups_page.find_message_and_dismiss(messages.SUCCESS))
-        self.assertFalse(
-            securitygroups_page.find_message_and_dismiss(messages.ERROR))
-        self.assertTrue(securitygroups_page.is_securitygroup_present(
-            self.SECURITYGROUP_NAME))
-
-        securitygroups_page.delete_securitygroup(self.SECURITYGROUP_NAME)
-        self.assertTrue(
-            securitygroups_page.find_message_and_dismiss(messages.SUCCESS))
-        self.assertFalse(
-            securitygroups_page.find_message_and_dismiss(messages.ERROR))
-        self.assertFalse(securitygroups_page.is_securitygroup_present(
-            self.SECURITYGROUP_NAME))
+    def test_managerules_create_delete_by_table(self):
+        """tests the manage rules creation and deletion funcionalities:
+        * create a new security group
+        * verifies the security group appears in the security groups table
+        * creates a new rule
+        * verifies the rule appears in the rules table
+        * delete the newly created rule
+        * verifies the rule does not appear in the table after deletion
+        * deletes the newly created security group
+        * verifies the security group does not appear in the table after
+        deletion
+        """
+        self._create_securitygroup()
+        self._add_rule()
+        self._delete_rule_by_table_action()
+        self._delete_securitygroup()
