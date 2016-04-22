79fcc65491052adf50ffe141997c28b658a0461b
Sergei Chipiga <schipiga@mirantis.com>
2016-04-22 13:00:16 +0300

    Fix pre_test_hook fail and sidebar menu fail
    It fixes two small but important bugs:
    - remove apache restart, because it fails pre_test_hook, which fails
      test build (the latter implication was introduced recently in a
      devstack-gate scripts);
    - fix sidebar menu item click before prev item was collapsed, because
      in this case menu item won't be expanded (supposedly a result of
      recent update of one of XStatic dependencies).
    
    Change-Id: If463c4c8b16b54f033e2a2ee33ba995ca7844f2b
    Closes-Bug: #1573726

--- tools/gate/integration/pre_test_hook.sh
+++ tools/gate/integration/pre_test_hook.sh
@@ -6,4 +6,3 @@ set -x
 
 cd /opt/stack/new/horizon/openstack_dashboard/local/local_settings.d
 mv _20_integration_tests_scaffolds.py.example _20_integration_tests_scaffolds.py
-sudo service apache2 restart
