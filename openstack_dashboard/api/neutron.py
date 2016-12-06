1b153ca9829e877111c3deabe8b4d1729408e26d
Matt Borland <matt.borland@hpe.com>
2016-12-06 08:28:45 -0700

    Allow router panel to not break sidebar
    This patch handles an exception that is raised when the network service
    is not present.  When unhandled, this causes the sidebar to not exist.
    
    Change-Id: Ic5a94b267158d17a145c64c75fd7bf88e98478fa
    Closes-Bug: 1647766

--- openstack_dashboard/api/neutron.py
+++ openstack_dashboard/api/neutron.py
@@ -1263,7 +1263,10 @@ def _server_get_addresses(request, server, ports, floating_ips, network_names):
 @profiler.trace
 @memoized
 def list_extensions(request):
-    extensions_list = neutronclient(request).list_extensions()
+    try:
+        extensions_list = neutronclient(request).list_extensions()
+    except exceptions.ServiceCatalogException:
+        return {}
     if 'extensions' in extensions_list:
         return tuple(extensions_list['extensions'])
     else:
