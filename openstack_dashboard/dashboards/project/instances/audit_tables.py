0d0a872a526003290a9105fa6b5d1fc975c628d9
Ying Zuo <yinzuo@cisco.com>
2017-02-22 15:34:01 -0800

    Remove link for user id field on project action log
    The action log under project dashboard should not link
    to the user details panel which requires admin permission.
    
    Change-Id: I802188db6f1f8fc7e854980eca85d3b1a75aec1e
    Closes-bug: #1667154
    (cherry picked from commit 804db52d7dd6ff5f0d6c6ed9a5196f0d399357e6)

--- openstack_dashboard/dashboards/project/instances/audit_tables.py
+++ openstack_dashboard/dashboards/project/instances/audit_tables.py
@@ -13,17 +13,11 @@
 #   under the License.
 
 
-from django.core import urlresolvers
 from django.utils.translation import pgettext_lazy
 from django.utils.translation import ugettext_lazy as _
 
 from horizon import tables
 from horizon.utils import filters
-
-
-def user_link(datum):
-    return urlresolvers.reverse("horizon:identity:users:detail",
-                                args=(datum.user_id,))
 
 
 class AuditTable(tables.DataTable):
@@ -48,8 +42,7 @@ class AuditTable(tables.DataTable):
                            display_choices=ACTION_DISPLAY_CHOICES)
     start_time = tables.Column('start_time', verbose_name=_('Start Time'),
                                filters=[filters.parse_isotime])
-    user_id = tables.Column('user_id', verbose_name=_('User ID'),
-                            link=user_link)
+    user_id = tables.Column('user_id', verbose_name=_('User ID'))
     message = tables.Column('message', verbose_name=_('Message'))
 
     class Meta(object):
