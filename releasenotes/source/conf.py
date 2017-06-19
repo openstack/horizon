ae12dc4f09c625a8ece79b4d51bfaf2f5e88b8f7
Doug Hellmann <doug@doughellmann.com>
2017-06-19 13:56:25 -0400

    switch release notes build to openstackdocstheme
    Change-Id: I6e0f74fa31d9e319267d81e3437a834689bb9f19
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -29,6 +29,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
 
+import openstackdocstheme
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -38,7 +40,6 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'oslosphinx',
     'reno.sphinxext',
     'sphinx.ext.extlinks',
 ]
@@ -128,7 +129,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -137,6 +138,7 @@ html_theme = 'default'
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
+html_theme_path = [openstackdocstheme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
