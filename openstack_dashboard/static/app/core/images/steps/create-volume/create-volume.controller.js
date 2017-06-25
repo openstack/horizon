2096599130df1439331d78c2ed532417e0d8cab8
wei.ying <wei.ying@easystack.cn>
2017-06-26 00:36:31 +0800

    Correct the form validation in ng-image create volume form
    The field of "Name" on Horizon dashboard in process of
    create volume from mandatory to optional. Also volume name and
    description should not be greater than 255 characters.
    
    Change-Id: I960f289a2f8d87d31b9b63ab072f994aead0eaf2
    Closes-Bug: #1700378

--- openstack_dashboard/static/app/core/images/steps/create-volume/create-volume.controller.js
+++ openstack_dashboard/static/app/core/images/steps/create-volume/create-volume.controller.js
@@ -27,6 +27,7 @@
     'horizon.app.core.openstack-service-api.cinder',
     'horizon.app.core.openstack-service-api.nova',
     'horizon.framework.widgets.charts.quotaChartDefaults',
+    'horizon.app.core.images.validationRules',
     'BYTE_TO_GIB'
   ];
 
@@ -38,11 +39,20 @@
    * @param {Object} cinder
    * @param {Object} nova
    * @param {Object} quotaChartDefaults
+   * @param {Object} validationRules
    * @description
    * This controller is use for creating an image.
    * @return {undefined} No return value
    */
-  function CreateVolumeController($scope, $filter, cinder, nova, quotaChartDefaults, BYTE_TO_GIB) {
+  function CreateVolumeController(
+    $scope,
+    $filter,
+    cinder,
+    nova,
+    quotaChartDefaults,
+    validationRules,
+    BYTE_TO_GIB
+   ) {
     var ctrl = this;
 
     ctrl.volumeType = {};
@@ -50,6 +60,7 @@
     ctrl.availabilityZones = [];
     ctrl.image = $scope.image;
     ctrl.sourceImage = getSourceImage(ctrl.image);
+    ctrl.validationRules = validationRules;
     ctrl.maxTotalVolumeGigabytes = 100;
     ctrl.totalGigabytesUsed = 0;
     ctrl.maxTotalVolumes = 1;
