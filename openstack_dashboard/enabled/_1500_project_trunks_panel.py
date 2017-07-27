7aad32e83ebea1d134f31ee3678a0b7c87764eda
Bence Romsics <bence.romsics@ericsson.com>
2017-07-27 09:37:00 +0200

    Trunks panel: default to disabled
    Unfortunately we missed to complete the trunks panel until the Pike
    freeze. Since it is incomplete we better disable it by default.
    The merged part is working though, users can view and delete trunks.
    The panel can be re-enabled by deleting the DISABLED flag from the
    file enabling the panel:
    
    openstack_dashboard/enabled/_1500_project_trunks_panel.py
    
    Change-Id: I956c10666f0449478e3489f13d7eff049ad95890
    Partially-Implements: blueprint neutron-trunk-ui

--- openstack_dashboard/enabled/_1500_project_trunks_panel.py
+++ openstack_dashboard/enabled/_1500_project_trunks_panel.py
@@ -1,3 +1,17 @@
+# Copyright 2017 Ericsson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # The slug of the panel to be added to HORIZON_CONFIG. Required.
 PANEL = 'trunks'
 # The slug of the dashboard the PANEL associated with. Required.
@@ -8,3 +22,6 @@ PANEL_GROUP = 'network'
 # Python panel class of the PANEL to be added.
 ADD_PANEL = \
     'openstack_dashboard.dashboards.project.trunks.panel.Trunks'
+
+# Will default to disabled until the feature is completed.
+DISABLED = True
