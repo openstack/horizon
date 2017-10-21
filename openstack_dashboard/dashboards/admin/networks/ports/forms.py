09efe2d432c7a17c696e7240f69b3c76b481e3f3
Akihiro Motoki <amotoki@gmail.com>
2017-10-21 00:48:42 +0000

    Allow regular users to specify VNIC type for port
    VNIC type needs to be specified when creating a server with an SR-IOV
    port. In this case, a user first creates a port with vnic_type=direct
    and creates a server with the created port. To allow this scenario,
    horizon needs to allow users to specify VNIC type for port.
    
    This commit also clean up the code duplication on the port security and
    mac state fields in the port create form. The code in the admin form for
    them are completely duplicated with that in the project form.
    
    Change-Id: Ib6e91ed7f720e2720994025429da0dcef2fa4e25
    Closes-Bug: #1402959

--- openstack_dashboard/dashboards/admin/networks/ports/forms.py
+++ openstack_dashboard/dashboards/admin/networks/ports/forms.py
@@ -14,7 +14,6 @@
 
 import logging
 
-from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -38,56 +37,6 @@ class CreatePort(project_forms.CreatePort):
                     "hosts."),
         required=False)
     failure_url = 'horizon:admin:networks:detail'
-
-    def __init__(self, request, *args, **kwargs):
-        super(CreatePort, self).__init__(request, *args, **kwargs)
-
-        try:
-            if api.neutron.is_extension_supported(request, 'binding'):
-                neutron_settings = getattr(settings,
-                                           'OPENSTACK_NEUTRON_NETWORK', {})
-                supported_vnic_types = neutron_settings.get(
-                    'supported_vnic_types', ['*'])
-                if supported_vnic_types:
-                    if supported_vnic_types == ['*']:
-                        vnic_type_choices = api.neutron.VNIC_TYPES
-                    else:
-                        vnic_type_choices = [
-                            vnic_type for vnic_type in api.neutron.VNIC_TYPES
-                            if vnic_type[0] in supported_vnic_types
-                        ]
-
-                    self.fields['binding__vnic_type'] = \
-                        forms.ThemableChoiceField(
-                            choices=vnic_type_choices,
-                            label=_("Binding: VNIC Type"),
-                            help_text=_(
-                                "The VNIC type that is bound to the neutron "
-                                "port"),
-                            required=False)
-        except Exception:
-            msg = _("Unable to verify the VNIC types extension in Neutron")
-            exceptions.handle(self.request, msg)
-
-        try:
-            if api.neutron.is_extension_supported(request, 'mac-learning'):
-                self.fields['mac_state'] = forms.BooleanField(
-                    label=_("MAC Learning State"), initial=False,
-                    required=False)
-        except Exception:
-            msg = _("Unable to retrieve MAC learning state")
-            exceptions.handle(self.request, msg)
-
-        try:
-            if api.neutron.is_extension_supported(request, 'port-security'):
-                self.fields['port_security_enabled'] = forms.BooleanField(
-                    label=_("Port Security"),
-                    help_text=_("Enable anti-spoofing rules for the port"),
-                    initial=True,
-                    required=False)
-        except Exception:
-            msg = _("Unable to retrieve port security state")
-            exceptions.handle(self.request, msg)
 
     def handle(self, request, data):
         network_id = self.initial['network_id']
