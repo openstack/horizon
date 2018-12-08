3143edef0131b82a1d8dea8ab82e32d0225779ee
Akihiro Motoki <amotoki@gmail.com>
2018-12-09 02:57:26 +0900

    pylint: fix import-error
    It seems pylint cannot handle distutils imports
    and shows "import-error" error. I believe it sounds
    reasonable to ignore this case.
    
    Change-Id: Ia08b4fbf8c3270aa37cd88967add8258f6b26377

--- openstack_dashboard/hooks.py
+++ openstack_dashboard/hooks.py
@@ -13,7 +13,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-# pylint: disable=no-name-in-module
+# pylint: disable=no-name-in-module,import-error
 from distutils.command import install
 
 
