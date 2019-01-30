ca1e8f77ccbe502534412f20ff77c5c5033bc62c
jmoffitt <jeremy.moffitt@suse.com>
2019-01-30 13:26:22 -0800

    Consistency Group Snapshots detail url is wrong
    The link in the consistency group table for "detail" is a mismatch
    for the corresponding value in the urls.py file, causing cg snapshots
    clicks to not load details for the user.
    
    This bug was introduced in https://review.openstack.org/#/c/429506
    where the link in tables.py was changed from:
    "horizon:project:volumes:cg_snapshots:cg_snapshot_detail"
    to
    "horizon:project:cg_snapshots:detail"
    
    the removal of the volumes section of the link was correct for that
    change, but changing "cg_snapshot_detail" to "detail" broke the
    details link
    
    Change-Id: I7ad8673bad353a234596df16c8e64eec93733934
    Closes-Bug: #1813990
    (cherry picked from commit 468184bd9754f41c072ee4afc54ca6f9a0de9af5)

--- openstack_dashboard/dashboards/project/cg_snapshots/tables.py
+++ openstack_dashboard/dashboards/project/cg_snapshots/tables.py
@@ -90,7 +90,8 @@ class CGSnapshotsTable(tables.DataTable):
 
     name = tables.Column("name",
                          verbose_name=_("Name"),
-                         link="horizon:project:cg_snapshots:detail")
+                         link="horizon:project:"
+                              "cg_snapshots:cg_snapshot_detail")
     description = tables.Column("description",
                                 verbose_name=_("Description"),
                                 truncate=40)
