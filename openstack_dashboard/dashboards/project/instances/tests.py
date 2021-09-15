ec39137c2d23b0da3bdc7a7ffcd0997a18f4c5ca
Nicolas Bock <nicolas.bock@canonical.com>
2021-09-15 09:39:31 -0600

    Get ports directly instead of via loop
    In order to get a list of available ports, the current implementation
    of the form code in `project/instances/attach_interface` loops through
    the available networks in the project and requests a list of ports for
    each via the Neutron API. This implementation is O(N) in time with a
    large prefactor (the time for a Neutron API call) where N is the
    number of networks.
    
    Instead of calling the Neutron API for each network this change uses
    one call to the Neutron API to get the list of ports on all networks
    and filters this list via a list comprehension. While this
    implementation is still O(N) the prefactor is significantly smaller.
    
    Includes some unit test fixes to workaround the fact that patch
    6ac31e0 is not backported beyond Y.
    
    Closes-Bug: #1943639
    Change-Id: I8fd32c3aad22d8ef7f57201f5144f6b2e357ef10
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>
    (cherry picked from commit 9f5d659d160d24d359992dda22fecb0db6b6f805)
    (cherry picked from commit 1561fcce55371c6b2d93a31b83ca533dea82f746)

--- openstack_dashboard/dashboards/project/instances/tests.py
+++ openstack_dashboard/dashboards/project/instances/tests.py
@@ -148,13 +148,9 @@ class InstanceTableTestMixin(object):
                       shared=False),
             mock.call(helpers.IsHttpRequest(), shared=True),
         ])
-        self.assertEqual(len(self.networks.list()),
-                         self.mock_port_list_with_trunk_types.call_count)
-        self.mock_port_list_with_trunk_types(
-            [mock.call(helpers.IsHttpRequest(),
-                       network_id=net.id,
-                       tenant_id=self.tenant.id)
-             for net in self.networks.list()])
+        self.mock_port_list_with_trunk_types.assert_called_once_with(
+            helpers.IsHttpRequest(),
+            tenant_id=self.tenant.id)
 
     def _mock_nova_lists(self):
         self.mock_flavor_list.return_value = self.flavors.list()
@@ -2348,12 +2344,9 @@ class InstanceLaunchInstanceTests(InstanceTestBase,
             mock.call(helpers.IsHttpRequest(), shared=True),
         ])
         self.assertEqual(4, self.mock_network_list.call_count)
-        self.mock_port_list_with_trunk_types.assert_has_calls(
-            [mock.call(helpers.IsHttpRequest(),
-                       network_id=net.id, tenant_id=self.tenant.id)
-             for net in self.networks.list()])
-        self.assertEqual(len(self.networks.list()),
-                         self.mock_port_list_with_trunk_types.call_count)
+        self.mock_port_list_with_trunk_types.assert_called_once_with(
+            helpers.IsHttpRequest(),
+            tenant_id=self.tenant.id)
         self.mock_server_group_list.assert_called_once_with(
             helpers.IsHttpRequest())
         self.mock_tenant_quota_usages.assert_called_once_with(
@@ -2525,10 +2518,9 @@ class InstanceLaunchInstanceTests(InstanceTestBase,
             mock.call(helpers.IsHttpRequest(), shared=True),
         ])
         self.assertEqual(4, self.mock_network_list.call_count)
-        self.mock_port_list_with_trunk_types.assert_has_calls(
-            [mock.call(helpers.IsHttpRequest(),
-                       network_id=net.id, tenant_id=self.tenant.id)
-             for net in self.networks.list()])
+        self.mock_port_list_with_trunk_types.assert_called_once_with(
+            helpers.IsHttpRequest(),
+            tenant_id=self.tenant.id)
         self.mock_server_group_list.assert_called_once_with(
             helpers.IsHttpRequest())
         self.mock_tenant_quota_usages.assert_called_once_with(
@@ -2621,10 +2613,9 @@ class InstanceLaunchInstanceTests(InstanceTestBase,
             mock.call(helpers.IsHttpRequest(), shared=True),
         ])
         self.assertEqual(4, self.mock_network_list.call_count)
-        self.mock_port_list_with_trunk_types.assert_has_calls(
-            [mock.call(helpers.IsHttpRequest(),
-                       network_id=net.id, tenant_id=self.tenant.id)
-             for net in self.networks.list()])
+        self.mock_port_list_with_trunk_types.assert_called_once_with(
+            helpers.IsHttpRequest(),
+            tenant_id=self.tenant.id)
         self.mock_server_group_list.assert_called_once_with(
             helpers.IsHttpRequest())
         self.mock_tenant_quota_usages.assert_called_once_with(
@@ -4006,13 +3997,9 @@ class InstanceLaunchInstanceTests(InstanceTestBase,
                 helpers.IsHttpRequest(),
                 shared=True),
         ])
-        self.assertEqual(len(self.networks.list()),
-                         self.mock_port_list_with_trunk_types.call_count)
-        self.mock_port_list_with_trunk_types.assert_has_calls(
-            [mock.call(helpers.IsHttpRequest(),
-                       network_id=net.id,
-                       tenant_id=self.tenant.id)
-             for net in self.networks.list()])
+        self.mock_port_list_with_trunk_types.assert_called_once_with(
+            helpers.IsHttpRequest(),
+            tenant_id=self.tenant.id)
         self.mock_volume_list.assert_has_calls([
             mock.call(helpers.IsHttpRequest(),
                       search_opts=VOLUME_SEARCH_OPTS),
@@ -5608,7 +5595,8 @@ class ConsoleManagerTests(helpers.ResetImageAPIVersionMixin, helpers.TestCase):
         self.assertRaises(exceptions.NotAvailable,
                           console.get_console, None, 'FAKE', None)
 
-    @helpers.create_mocks({api.neutron: ('network_list_for_tenant',)})
+    @helpers.create_mocks({api.neutron: ('network_list_for_tenant',
+                                         'port_list_with_trunk_types',)})
     def test_interface_attach_get(self):
         server = self.servers.first()
         self.mock_network_list_for_tenant.side_effect = [
@@ -5628,7 +5616,8 @@ class ConsoleManagerTests(helpers.ResetImageAPIVersionMixin, helpers.TestCase):
         ])
         self.assertEqual(2, self.mock_network_list_for_tenant.call_count)
 
-    @helpers.create_mocks({api.neutron: ('network_list_for_tenant',),
+    @helpers.create_mocks({api.neutron: ('network_list_for_tenant',
+                                         'port_list_with_trunk_types',),
                            api.nova: ('interface_attach',)})
     def test_interface_attach_post(self):
         fixed_ip = '10.0.0.10'
