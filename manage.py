openstack/horizon

commit e85356dfd7d5ff98eb370dda9cfac6c11f310e8f

Fix manage.py to use python3
Looks like we still have some py2 legacy sheebang that we need fixing.

Change-Id: I44a93385c201ddcd3f5f5de30b8191e401188388

manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain

commit c14f9f714746c5760c66c0cea682e5b58a86b71a

Fix runtime detection in virtualenv
The previous change e85356dfd7d5ff98eb370dda9cfac6c11f310e8f broke
detection in virtualenv.

Change-Id: If403d134122c4b55e653057a9eb6c469bc66b39e

manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain