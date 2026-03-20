9598d44471ffe84d9796bea8f636e62a63aed021
Tatiana Ovchinnikova <t.v.ovtchinnikova@gmail.com>
2026-03-20 16:08:44 -0500

    Don't request flavors extra specs for Launch Instance dialog
    There's no need to request flavors extra specs for displaying
    list of flavors in Launch Instance dialog. When Nova API endpoint
    is slow or number of flavors is high it can cause significant
    delays.
    
    Change-Id: I352225a9df7d1406f7d5982fd7338ca2973a0564
    Signed-off-by: Tatiana Ovchinnikova <t.v.ovtchinnikova@gmail.com>
    (cherry picked from commit 6922d03e51bdb77cff85e0c642b7fc372df335be)

--- openstack_dashboard/dashboards/project/static/dashboard/project/workflow/launch-instance/launch-instance-model.service.js
+++ openstack_dashboard/dashboards/project/static/dashboard/project/workflow/launch-instance/launch-instance-model.service.js
@@ -261,7 +261,7 @@
             .finally(onGetAvailabilityZonesComplete),
           novaAPI.getFlavors({
             is_public: true,
-            get_extras: true
+            get_extras: false
           }).then(onGetFlavors, noop),
           novaAPI.getKeypairs().then(onGetKeypairs, noop),
           novaAPI.getLimits(true).then(onGetNovaLimits, noop),
